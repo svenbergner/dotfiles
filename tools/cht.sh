1f04f3d69b6d4761e13704822e363700cad6b563
Sven Bergner <sven.bergner@wolterskluwer.com>
2022-09-23 13:38:04 +0200

    First commit: Script for looking up cheat sheet

---
+++ tools/cht.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+
+languages="flutter dart cpp javascript html css bash"
+core_utils="xargs find fzf mv sed awk"
+
+list=""
+
+for element in $languages $core_utils
+do
+    if [[ -z $list ]]
+    then
+        list="$element"
+    else
+        list="$list\n$element"
+    fi
+done
+
+selected=`echo $list | fzf`
+read -p "query: " query
+query=`echo $query | tr ' ' '+'`
+
+if [[ $languages =~ $selected ]]
+then
+    curl cht.sh/$selected/$query | bat --paging=always
+elif [[ $core_utils =~ $selected ]]
+then
+    curl cht.sh/$selected~$query | bat --paging=always
+else
+    echo "Error: Selected element is unknown!"
+fi
+
