1710a0eeba83eda505cdb74ecb16399730ed347b
Sven Bergner <sven.bergner@gmail.com>
2023-11-14 10:24:23 +0100

    Added some options

---
+++ nvim/lua/options/init.lua
@@ -0,0 +1,9 @@
+-- let $LANG='en'
+
+vim.opt.guicursor = "n-v-c:block-Cursor/lCursor, ve:ver35-Cursor, o:hor50-Cursor, i-ci:ver25-Cursor/lCursor, r-cr:hor20-Cursor/lCursor, sm:block-Cursor -blinkwait175-blinkoff150-blinkon175"
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+
