e5329c9528c0118be1696fff7f89b709db4c73fe
Sven Bergner <sven.bergner@wolterskluwer.com>
2023-11-14 10:23:27 +0100

    Added some split keymaps

---
+++ nvim/lua/keymaps/init.lua
@@ -0,0 +1,17 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.g.backspace = "indent,eol,start"
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("i", "jk", "<ESC>")
+
+vim.g.splitbelow = true
+vim.g.splitright = true
+
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+
