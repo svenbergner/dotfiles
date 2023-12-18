f37e02837a0a1b0d9e9184a7fac548353d549997
Sven Bergner <sven.bergner@gmail.com>
2023-12-18 16:31:33 +0100

    adjusted tabstop

--- nvim/lua/options/init.lua
+++ nvim/lua/options/init.lua
@@ -5,7 +5,7 @@ vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50"
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
+vim.opt.tabstop = 2
 
 vim.opt.cursorline = false
 vim.opt.syntax = on
