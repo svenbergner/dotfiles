3988e3bceed58e864226a0aacf5f39910eaf0823
Sven Bergner <sven.bergner@gmail.com>
2023-12-21 10:52:45 +0100

    Moved treesitter plugin into a seperate file

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+        local configs = require('nvim-treesitter.configs')
+        configs.setup({
+            ensure_installed = { 'lua', 'vim', 'bash', 'dart', 'html', 'css', 'yaml' },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end
+}
