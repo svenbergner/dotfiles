852f730a6322d05e585c061afb959a05c5ff6496
Sven Bergner <sven.bergner@gmail.com>
2023-12-22 09:27:38 +0100

    Added keymaps for resizing splits

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -17,11 +17,23 @@ vim.keymap.set("i", "jk", "<ESC>")
 vim.g.splitbelow = true
 vim.g.splitright = true
 
+-- Move between splits with CTRL + hjkl
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
+-- Make adjusting split sizes a bit more friendly
+vim.keymap.set("n", "<C-Left>", ":vertical resize +1<CR>")
+vim.keymap.set("n", "<C-Right>", ":vertical resize +1<CR>")
+vim.keymap.set("n", "<C-Up>", ":resize +1<CR>")
+vim.keymap.set("n", "<C-Down>", ":resize +1<CR>")
+
+-- Change 2 split windows from vert to horiz or horiz to vert
+vim.keymap.set("n", "<leader>th", "<C-w>t<C-w>H")
+vim.keymap.set("n", "<leader>tk", "<C-w>t<C-w>K")
+
+-- Create and change tabs
 vim.keymap.set("n", "te", ":tabedit")
 vim.keymap.set("n", "<tab>", ":tabnext<Return>")
 vim.keymap.set("n", "<s-tab>", ":tabprev<Return>")
