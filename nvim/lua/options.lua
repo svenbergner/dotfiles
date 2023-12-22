2e2385f8770dda254723ae16f1ce82f04aa0211d
Sven Bergner <sven.bergner@gmail.com>
2023-12-22 14:42:46 +0100

    Some options added

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,24 +1,35 @@
 -- Set vim language explicitly to english for easier usage with tutorials
 vim.api.nvim_exec ('language en_US', true)
 
+vim.opt.compatible = false
+
 vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50"
 
 -- Show current line number and relative line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- tab
+vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth=4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
+
+--
 vim.opt.hidden = true
+vim.opt.swapfile = false
+
+-- linewrap
 vim.opt.wrap = false
 vim.wo.wrap = false
 
+-- split behaviour
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+vim.opt.equalalways = true
 
 -- Highlight cursor line
 vim.opt.cursorline = true
@@ -28,6 +39,31 @@ vim.opt.syntax = on
 
 -- Show lineendings
 vim.opt.list = true
+vim.opt.listchars = { tab = '> ', trail = '-', eol = '↵' }
 
 vim.g.noerrorbells = true
 vim.g.nohlsearch = true
+
+-- file search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.history = 1000
+
+-- statusline
+vim.opt.laststatus = 2
+vim.opt.showcmd = true
+vim.opt.showmode = true -- change to false after 
+vim.opt.showmatch = true
+
+-- Wildmenu
+vim.opt.wildmenu = true
+vim.opt.wildmode = longest,full
+vim.opt.wildignore:append{'*.docx', '*.jpg', '*.png', '*.gif', '*.pdf', '*.pyc', '*.exe', '*.flv', '*.img', '*.xlsx'}
+
+-- Check for neovim equivalent
+-- Spellchecking
+-- set spelllang=de,en_gb,en_us
+-- set spell
