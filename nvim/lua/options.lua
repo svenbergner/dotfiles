80eb245ca8ae2983478ca6a615cb7330cb7f5da5
Sven Bergner <sven.bergner@gmail.com>
2023-12-22 09:44:20 +0100

    added some options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -3,11 +3,24 @@ vim.api.nvim_exec ('language en_US', true)
 
 vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50"
 
+-- Show current line number and relative line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth=4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.hidden = true
+vim.opt.wrap = false
+vim.wo.wrap = false
 
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Highlight cursor line
 vim.opt.cursorline = true
 
 -- Turn syntax highlighting on.
@@ -16,3 +29,5 @@ vim.opt.syntax = on
 -- Show lineendings
 vim.opt.list = true
 
+vim.g.noerrorbells = true
+vim.g.nohlsearch = true
