b3545177870b63aa6dfe8f08ea201767a27c8c20
Sven Bergner <sven.bergner@gmail.com>
2023-12-25 00:16:40 +0100

    Descriptions added

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Prevent x and the delete key from overriding what's in the clipboard.
-vim.keymap.set("n", "x", '"_x')
+vim.keymap.set("n", "x", '"_x', { desc = "Delete without yank" } )
 vim.keymap.set("n", "X", '"_X')
 vim.keymap.set("n", "<Del>", '"_x')
 
@@ -12,7 +12,7 @@ vim.keymap.set("n", "<LEADER>?", ":Maps<CR>")
 vim.g.backspace = "indent,eol,start"
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("i", "jk", "<ESC>", { desc = "ESC" } )
 
 vim.g.splitbelow = true
 vim.g.splitright = true
@@ -37,4 +37,7 @@ vim.keymap.set("n", "<leader>tk", "<C-w>t<C-w>K")
 vim.keymap.set("n", "te", ":tabedit")
 vim.keymap.set("n", "<tab>", ":tabnext<Return>")
 vim.keymap.set("n", "<s-tab>", ":tabprev<Return>")
+
+vim.keymap.set("v", "p", '"_dP')
+vim.keymap.set("n", "Y", "y$")
 
