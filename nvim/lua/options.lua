273f0d65eb18cf59872b3ea0e00a601190f0e32e
Sven Bergner <sven.bergner@gmail.com>
2023-12-28 23:10:47 +0100

    added some options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -18,6 +18,8 @@ vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 
+vim.opt.scrolloff=5
+
 --
 vim.opt.hidden = true
 vim.opt.swapfile = false
@@ -35,7 +37,7 @@ vim.opt.equalalways = true
 vim.opt.cursorline = true
 
 -- Turn syntax highlighting on.
-vim.opt.syntax = on
+vim.opt.syntax = "on"
 
 -- Show lineendings
 vim.opt.list = true
@@ -54,16 +56,28 @@ vim.opt.history = 1000
 
 -- statusline
 vim.opt.laststatus = 2
+vim.opt.cmdheight = 2
 vim.opt.showcmd = true
 vim.opt.showmode = true -- change to false after 
 vim.opt.showmatch = true
+vim.opt.timeoutlen = 1000
+vim.opt.ttimeoutlen = 100
+
+-- show signs on the left
+vim.opt.signcolumn = "yes"
 
 -- Wildmenu
 vim.opt.wildmenu = true
-vim.opt.wildmode = longest,full
+vim.opt.wildmode = "longest:full"
 vim.opt.wildignore:append{'*.docx', '*.jpg', '*.png', '*.gif', '*.pdf', '*.pyc', '*.exe', '*.flv', '*.img', '*.xlsx'}
 
 -- Check for neovim equivalent
 -- Spellchecking
 -- set spelllang=de,en_gb,en_us
 -- set spell
+
+-- Undo
+local prefix = vim.fn.expand("~/.config")
+vim.opt.undodir = { prefix .. "/nvim/.undo//" }
+vim.opt.undofile = true
+
