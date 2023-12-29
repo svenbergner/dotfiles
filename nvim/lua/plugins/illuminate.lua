f9f145c3bbe4720946a105647fa6c4763fdbb127
Sven Bergner <sven.bergner@gmail.com>
2023-12-29 22:04:54 +0100

    Illuminate plugin added

---
+++ nvim/lua/plugins/illuminate.lua
@@ -0,0 +1,14 @@
+return {
+    'RRethy/vim-illuminate',
+    config = function ()
+        require('illuminate').configure({
+            providers = {
+                'lsp',
+                'treesitter',
+                'regex',
+            },
+            under_cursor = true,
+            delay = 0,
+        })
+    end
+}
