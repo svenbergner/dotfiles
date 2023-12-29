6a340c291c7d13de773f045e2e98fb8c26808897
Sven Bergner <sven.bergner@gmail.com>
2023-12-29 20:09:02 +0100

    Simplified usage of plugins, so there is no need for lazy.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,21 @@
 require("options")
 require("keymaps")
-require("plugins.lazy")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    { import = 'plugins' },
+},{})
 
 vim.cmd([[colorscheme gruvbox]])
-
