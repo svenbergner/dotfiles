092af4b2f9bb3bd82a8733750156fa2b129bc40a
Sven Bergner <sven.bergner@gmail.com>
2024-01-02 23:35:50 +0100

    readability

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,6 +1,12 @@
 return {
     "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip", "hrsh7th/cmp-buffer" },
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "hrsh7th/cmp-buffer",
+        "rafamadriz/friendly-snippets",
+    },
     config = function()
         local cmp = require("cmp")
         local luasnip = require("luasnip")
