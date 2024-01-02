d1ca179f930b549f1a3698dabd1a5114c92cfe22
Sven Bergner <sven.bergner@gmail.com>
2024-01-02 23:36:32 +0100

    unused tools removed

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -5,14 +5,10 @@ return {
         null_ls.setup({
             sources = {
                 null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.prettier,
                 null_ls.builtins.diagnostics.eslint_d,
-                null_ls.builtins.diagnostics.rubocop,
-                null_ls.builtins.formatting.rubocop,
             },
         })
 
         vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
     end,
 }
-
