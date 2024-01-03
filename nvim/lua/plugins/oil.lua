7b0550463f9393de4d1a8c42bd7d7b91fffa7d09
Sven Bergner <sven.bergner@gmail.com>
2024-01-03 21:46:24 +0100

    Plugin Oil added

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,33 @@
+return {
+    "stevearc/oil.nvim",
+    lazy = true,
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("oil").setup({
+            keymaps = {
+                ["<leader>v"] = "actions.select_vsplit",
+                ["<leader>s"] = "actions.select_split",
+                ["<leader>p"] = "actions.preview",
+                ["<leader>r"] = "actions.refresh",
+                ["<leader>t"] = "actions.select_tab",
+            },
+        })
+        vim.keymap.set("n", "<leader>tt", function()
+            vim.api.nvim_command("Oil")
+        end, { desc = "[T]oggle Filetree" })
+
+--[[ Auto open Oil if no file is open
+        local noFile = true
+        for _, buffer in ipairs(vim.fn.getbufinfo()) do
+            if string.len(buffer.name) ~= 0 then
+                noFile = false
+                break
+            end
+        end
+        if noFile then
+            vim.api.nvim_command("Oil")
+        end
+]]--
+    end,
+}
