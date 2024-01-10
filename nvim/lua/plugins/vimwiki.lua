1c9cc4484a16d05943466c75b904c06f4ae65328
Sven Bergner <sven.bergner@gmail.com>
2024-01-10 22:48:38 +0100

    calender plugin added

--- nvim/lua/plugins/vimwiki.lua
+++ nvim/lua/plugins/vimwiki.lua
@@ -1,6 +1,9 @@
 return {
-	"vimwiki/vimwiki",
-    config = function ()
+    "vimwiki/vimwiki",
+    dependencies = {
+        'mattn/calendar-vim',
+    },
+    config = function()
         vim.g.vimwiki_list = {
             {
                 path = '~/Repos/vimwiki',
@@ -13,5 +16,31 @@ return {
                 ext = '.md'
             },
         }
+
+        local api = vim.api
+        api.nvim_create_user_command('Diary', 'VimwikiDiaryIndex', {})
+
+        local vimwikigroup = api.nvim_create_augroup('vimwikigroup', { clear = true })
+
+        api.nvim_create_autocmd("FileType", {
+            group = vimwikigroup,
+            pattern = "vimwiki",
+            command = 'setlocal shiftwidth=6 tabstop=6 noexpandtab'
+        })
+
+        -- automatically update links on read diary
+        api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+            group = vimwikigroup,
+            pattern = "diary.wiki",
+            command = "VimwikiDiaryGenerateLinks",
+        })
+
+        -- insert diary entry header
+        api.nvim_create_autocmd("BufNewFile", {
+            group = vimwikigroup,
+            pattern = "*/diary/[0-9]*.(wiki|md)",
+            command = 'call append(0,[ "# " . strftime("%d.%m.%Y"), "", "## Tagebuch", "", "", "## Erkenntnis des Tages", "" ])',
+        })
     end
 }
+
