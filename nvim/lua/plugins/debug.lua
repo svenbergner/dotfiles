595e3f606720285addebb03bac2a292d2d2cee67
Sven Bergner <sven.bergner@gmail.com>
2024-01-11 14:36:42 +0100

    added bash debugging

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -9,6 +9,12 @@ return {
         local dap = require("dap")
         local dapui = require("dapui")
 
+        dap.adapters.bashdb = {
+            type = 'executable',
+            command = vim.fn.stdpath("data") .. '/mason/packages/bash-debug-adapter/bash-debug-adapter',
+            name = 'bashdb',
+        }
+
         dap.adapters.lldb = {
             name = "lldb",
             type = "server",
@@ -39,7 +45,7 @@ return {
         dap.adapters.dart = {
             type = "executable",
             command = "dart",
-            args = {"debug_adapter"}
+            args = { "debug_adapter" }
         }
         dap.configurations.dart = {
             {
@@ -49,6 +55,28 @@ return {
                 program = "${file}",
                 cwd = "${workspaceFolder}",
                 args = {}, -- Note for Dart Apps this is args, for Flutter apps toolArgs
+            }
+        }
+
+        dap.configurations.sh = {
+            {
+                type = 'bashdb',
+                request = 'launch',
+                name = "Launch file",
+                showDebugOutput = true,
+                pathBashdb = vim.fn.stdpath("data") .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb',
+                pathBashdbLib = vim.fn.stdpath("data") .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir',
+                trace = true,
+                file = "${file}",
+                program = "${file}",
+                cwd = '${workspaceFolder}',
+                pathCat = "cat",
+                pathBash = "/usr/local/bin/bash",
+                pathMkfifo = "mkfifo",
+                pathPkill = "pkill",
+                args = {},
+                env = {},
+                terminalKind = "integrated",
             }
         }
 
@@ -84,16 +112,19 @@ return {
 
         vim.fn.sign_define("DapStopped", { text = "▶️", texthl = "", linehl = "", numhl = "" })
 
-        vim.keymap.set("n", "<F5>", dap.continue, { desc = 'Start/Continue Debugging' } )
-        vim.keymap.set("n", "S-<F5>", dap.terminate, { desc = 'Stop Debugging' } )
-        vim.keymap.set("n", "C-S-<F5>", dap.run_last, { desc = 'Restart Debugging' } )
-        vim.keymap.set("n", "<F10>", dap.step_over, { desc = 'Step over' } )
-        vim.keymap.set("n", "<F11>", dap.step_into, { desc = 'Step into' } )
-        vim.keymap.set("n", "S-<F11>", dap.step_out, { desc = 'Step out' } )
-        vim.keymap.set("n", "<F9>", dap.toggle_breakpoint, { desc = 'Toggle breakpoint' } )
+        vim.keymap.set("n", "<F5>", dap.continue, { desc = 'Start/Continue Debugging' })
+        vim.keymap.set("n", "<S-F5>", dap.terminate, { desc = 'Stop Debugging' })
+        vim.keymap.set("n", "<C-S-F5>", dap.run_last, { desc = 'Restart Debugging' })
+        vim.keymap.set("n", "<F10>", dap.step_over, { desc = 'Step over' })
+        vim.keymap.set("n", "<F11>", dap.step_into, { desc = 'Step into' })
+        vim.keymap.set("n", "<S-F11>", dap.step_out, { desc = 'Step out' })
+        vim.keymap.set("n", "<F9>", dap.toggle_breakpoint, { desc = 'Toggle breakpoint' })
         vim.keymap.set("n", "<leader>B", function()
             dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-        end, { desc = 'Set conditional breakpoint' } )
+        end, { desc = 'Set conditional breakpoint' })
+
+        vim.keymap.set("n", "<S-F5>", function() print("<S-F5>") end)
+        vim.keymap.set("n", "<C-S-F5>", function() print("<C-S-F5>") end)
 
         dapui.setup({
             icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
