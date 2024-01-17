d872755c50152da2ce1364992b3cdeacfdf6f7b2
Sven Bergner <sven.bergner@wolterskluwer.com>
2024-01-17 11:38:34 +0100

    moved split options into options.lua and added some description

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,3 +1,4 @@
+-- Map leader and localleader to space
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -6,22 +7,21 @@ vim.keymap.set("n", "x", '"_x', { desc = "Delete char under cursor without yank"
 vim.keymap.set("n", "X", '"_X', { desc = "Delete char before cursor without yank" })
 vim.keymap.set("n", "<Del>", '"_x', { desc = "Delete char under cursor without yank" })
 
+-- Backspace deletes indent, end of line and 
 vim.g.backspace = "indent,eol,start"
 
+-- use jk as alternative for ESC key to go back to normal mode
 vim.keymap.set("i", "jk", "<ESC>", { desc = "ESC back to normal mode" })
-
-vim.g.splitbelow = true
-vim.g.splitright = true
 
 -- Switch Buffers
 vim.keymap.set("n", "<PageUp>", ":bp<CR>", { silent = true, desc = "Goto next buffer" })
 vim.keymap.set("n", "<PageDown>", ":bn<CR>", { silent = true, desc = "Goto previous buffer" })
 
 -- Move between splits with CTRL + hjkl
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to the left split" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to the down split" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to the upper split" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to the right split" })
 
 -- Make adjusting split sizes a bit more friendly
 vim.keymap.set("n", "<C-S-Left>", ":vertical resize -1<CR>", { silent = true, desc = "Increase current window width" })
@@ -30,16 +30,16 @@ vim.keymap.set("n", "<C-S-Up>", ":resize +1<CR>", { silent = true, desc = "Incre
 vim.keymap.set("n", "<C-S-Down>", ":resize -1<CR>", { silent = true, desc = "Decrease current window height" })
 
 -- Change 2 split windows from vert to horiz or horiz to vert
-vim.keymap.set("n", "<leader>th", "<C-w>t<C-w>H")
-vim.keymap.set("n", "<leader>tk", "<C-w>t<C-w>K")
+vim.keymap.set("n", "<leader>th", "<C-w>t<C-w>H", { desc = "" })
+vim.keymap.set("n", "<leader>tk", "<C-w>t<C-w>K", { desc = "" })
 
 -- Create and change tabs
 vim.keymap.set("n", "te", ":tabedit", { desc = "Open new tab" })
 vim.keymap.set("n", "<tab>", ":tabnext<Return>", { silent = true, desc = "Goto next tab" })
 vim.keymap.set("n", "<s-tab>", ":tabprev<Return>", { silent = true, desc = "Goto previous tab" })
 
-vim.keymap.set("v", "p", '"_dP')
-vim.keymap.set("n", "Y", "y$")
+vim.keymap.set("v", "p", '"_dP', { desc = "" })
+vim.keymap.set("n", "Y", "y$", { desc = "" })
 
 -- Move current line down and up
 vim.keymap.set("n", "<C-Down>", "ddp", { desc = "Move current line up" })
@@ -49,4 +49,5 @@ vim.keymap.set("n", "<C-Up>", "ddkP", { desc = "Move current line down" })
 vim.keymap.set("n", "<LEADER><LEADER>x", ":w<CR>:source %<CR>",
     { silent = true, desc = "Safe and source the current file." })
 
-vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+-- Format current buffer
+vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format current buffer" })
