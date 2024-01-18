785b4f5b77b6d52777e4b3618f5a5a763204bd55
Sven Bergner <sven.bergner@gmail.com>
2024-01-18 10:27:47 +0100

    Added window picker

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,16 +1,17 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-        "3rd/image.nvim",
-    },
-    config = function()
-        vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {silent = true})
-        vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {silent = true})
+   "nvim-neo-tree/neo-tree.nvim",
+   branch = "v3.x",
+   dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+      "3rd/image.nvim",
+      "s1n7ax/nvim-window-picker",
+   },
+   config = function()
+      vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", { silent = true })
+      vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", { silent = true })
 
-        require("neo-tree").setup({})
-    end,
+      require("neo-tree").setup({})
+   end,
 }
