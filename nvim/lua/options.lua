e720ef71c2b5455576bbbfa3cfe1422672682953
Sven Bergner <sven.bergner@gmail.com>
2024-01-18 10:05:00 +0100

    changed tabstop to match sourcecode

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -14,9 +14,9 @@ vim.opt.relativenumber = true
 vim.opt.virtualedit = "block"
 -- tab
 vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 3
+vim.opt.softtabstop = 3
+vim.opt.shiftwidth = 3
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 
