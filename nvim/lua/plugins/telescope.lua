2fee32cfbe63606e2c7582783b5453c4784ce003
Sven Bergner <sven.bergner@gmail.com>
2024-01-23 17:48:00 +0100

    Removed Advanced Git Search because it didn't work

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -17,7 +17,6 @@ return {
          vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find Buffers" })
          vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Find Help Tags" })
          vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = "Find Symbols" })
-         vim.keymap.set('n', '<leader>fi', '<cmd>AdvancedGitSearch<CR>', { desc = "AdvancedGitSearch" })
          vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = "Find Old Files" })
          vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = "Find Word under Cursor" })
          vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = "Search Git Commits" })
@@ -29,13 +28,9 @@ return {
       "nvim-telescope/telescope-live-grep-args.nvim",
       "nvim-telescope/telescope-ui-select.nvim",
       "debugloop/telescope-undo.nvim",
-      "aaronhallaert/advanced-git-search.nvim",
       config = function()
          require("telescope").setup({
             extensions = {
-               advanced_git_search = {
-
-               },
                ["ui-select"] = {
                   require("telescope.themes").get_dropdown({}),
                },
@@ -51,14 +46,8 @@ return {
          vim.g.zoxide_use_select = true
 
          require("telescope").load_extension("undo")
-         require("telescope").load_extension("advanced_git_search")
          require("telescope").load_extension("live_grep_args")
       end,
-      dependencies = {
-         "nvim-telescope/telescope.nvim",
-         "tpope/vim-fugitive",
-         "tpope/vim-rhubarb",
-      }
    },
    {
       'nvim-telescope/telescope-fzf-native.nvim',
