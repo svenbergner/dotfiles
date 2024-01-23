88cbad42041b53782801b0eccd0514e5eba59ae5
Sven Bergner <sven.bergner@gmail.com>
2024-01-23 17:47:30 +0100

    Fixed syntax

--- nvim/lua/plugins/nvim-neoclip.lua
+++ nvim/lua/plugins/nvim-neoclip.lua
@@ -1,7 +1,7 @@
 return {
    "AckslD/nvim-neoclip.lua",
-   requires = {
-      {'nvim-telescope/telescope.nvim'},
+   dependencies = {
+      'nvim-telescope/telescope.nvim',
    },
    config = function()
       require('neoclip').setup()
