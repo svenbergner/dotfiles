f65efdf1b8efcf2f1a551e1b064f7d0566eb106c
Sven Bergner <sven.bergner@gmail.com>
2024-01-23 17:46:58 +0100

    Removed because it didn't work

--- nvim/lua/plugins/advanced-git-search.lua
+++
@@ -1,8 +0,0 @@
-return {
-   "aaronhallaert/advanced-git-search.nvim",
-   dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "tpope/vim-fugitive",
-      "tpope/vim-rhubarb",
-   },
-}
