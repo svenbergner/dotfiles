31d4a1c1e380a04ef3f9b46ca938bbc8b2c82d5e
Sven Bergner <sven.bergner@gmail.com>
2024-01-24 09:51:29 +0100

    fixed my lsp config

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,76 +1,104 @@
 return {
-   {
-      "williamboman/mason.nvim",
-      config = function()
-         require("mason").setup()
-      end,
+   'neovim/nvim-lspconfig',
+   dependencies = {
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+      'j-hui/fidget.nvim',
+      'folke/neodev.nvim',
    },
-   {
-      "williamboman/mason-lspconfig.nvim",
-      config = function()
-         require("mason-lspconfig").setup({
-            ensure_installed = {
-               "lua_ls",
-               "ast_grep",
-               "bashls",
-               -- "clangd",
-               "cmake",
-               "dockerls",
-            },
-            automatic_installation = true,
-         })
-      end,
-   },
-   {
-      "neovim/nvim-lspconfig",
-      config = function()
-         local lspconfig = require("lspconfig")
-         lspconfig.lua_ls.setup({
-            on_init = function(client)
-               local path = client.workspace_folders[1].name
-               if
-                   not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc")
-               then
-                  client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
-                     Lua = {
-                        format = {
-                           enable = true,
-                           defaultConfig = {
-                              indent_style = "space",
-                           },
-                        },
-                        runtime = {
-                           -- Tell the language server which version of Lua you're using
-                           -- (most likely LuaJIT in the case of Neovim)
-                           version = "LuaJIT",
-                        },
-                        -- Make the server aware of Neovim runtime files
-                        workspace = {
-                           checkThirdParty = false,
-                           library = {
-                              vim.env.VIMRUNTIME,
-                              -- "${3rd}/luv/library"
-                              -- "${3rd}/busted/library",
-                           },
-                           -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                           -- library = vim.api.nvim_get_runtime_file("", true)
-                        },
-                     },
-                  })
+   config = function()
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+      vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+      vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
-                  client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+      vim.diagnostic.config({
+         float = {
+            source = 'always',
+            border = "single"
+         },
+      })
+
+      local on_attach = function(_, bufnr)
+         local nmap = function(keys, func, desc)
+            if desc then
+               desc = 'LSP: ' .. desc
+            end
+            vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+         end
+         local is_clangd = function()
+            for _, client in pairs(vim.lsp.get_active_clients { bufnr = bufnr }) do
+               if client.name == "clangd" then
+                  return true
                end
-               return true
-            end,
-         })
-         lspconfig.ast_grep.setup({})
-         lspconfig.bashls.setup({})
-         vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-         vim.keymap.set("n", "<leader>k", vim.lsp.buf.signature_help, {})
-         vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-         vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-         vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-         vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-      end,
-   },
+               return false
+            end
+         end
+         if is_clangd() then
+            local sw = require('lspconfig.server_configurations.clangd').commands.ClangdSwitchSourceHeader[1]
+            nmap('<F4>', sw, 'F4 - switch source/header')
+         end
+         nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+         nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+         nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+         nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+         nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+         nmap('<leader>Ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+         nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+         nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+         nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+         nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+         nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+         nmap('<leader>Wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+         nmap('<leader>Wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+         nmap('<leader>Wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+         end, '[W]orkspace [L]ist Folders')
+
+         vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+            vim.lsp.buf.format()
+         end, { desc = 'Format current buffer with LSP' })
+         nmap('<leader>df', vim.lsp.buf.format, 'LSP: format document')
+      end
+
+      local servers = {
+         clangd = {},
+         ast_grep = {},
+         lua_ls = {
+            Lua = {
+               workspace = { checkThirdParty = false },
+               telemetry = { enable = false },
+               diagnostics = { disable = { 'missing-fields' } },
+            },
+         },
+         bashls = {},
+         cmake = {},
+         dockerls = {},
+      }
+
+      require('neodev').setup()
+
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+      require('mason').setup()
+      local mason_lspconfig = require 'mason-lspconfig'
+      mason_lspconfig.setup {
+         ensure_installed = vim.tbl_keys(servers),
+         automatic_installation = true,
+      }
+      mason_lspconfig.setup_handlers {
+         function(server_name)
+            require('lspconfig')[server_name].setup {
+               capabilities = capabilities,
+               on_attach = on_attach,
+               settings = servers[server_name],
+               handlers = {
+                  ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'single' }),
+                  ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'single' })
+               },
+            }
+         end,
+      }
+   end
 }
