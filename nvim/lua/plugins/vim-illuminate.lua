e511d414a09de98c25b3ae021658998024914ced
Sven Bergner <sven.bergner@gmail.com>
2024-01-31 09:28:12 +0100

    Comment added

--- nvim/lua/plugins/vim-illuminate.lua
+++ nvim/lua/plugins/vim-illuminate.lua
@@ -1,3 +1,5 @@
+--  Neovim plugin for automatically highlighting other uses of the word
+--  under the cursor using either LSP, Tree-sitter, or regex matching.
 return {
    "RRethy/vim-illuminate",
    config = function()
