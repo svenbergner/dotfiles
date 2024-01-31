e26972268f18a0d5e0c333d4660865b8b8ecb250
Sven Bergner <sven.bergner@wolterskluwer.com>
2024-01-31 11:03:19 +0100

    Change line number color due to git changes

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -10,6 +10,7 @@ return {
             topdelete = { text = "‾" },
             changedelete = { text = "~" },
          },
+         numhl = true, -- Change color of line number 
       })
       vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "[G]it [p]review hunk" })
    end
