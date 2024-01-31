3f8c1b0d8fb48609e6deec1120d17120e8ec70ea
Sven Bergner <sven.bergner@gmail.com>
2024-01-31 10:42:50 +0100

    Word added

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,13 +1,16 @@
 return {
    "lewis6991/gitsigns.nvim",
-   opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-         add = { text = "+" },
-         change = { text = "~" },
-         delete = { text = "_" },
-         topdelete = { text = "‾" },
-         changedelete = { text = "~" },
-      },
-   },
+   config = function()
+      require("gitsigns").setup({
+         -- See `:help gitsigns.txt`
+         signs = {
+            add = { text = "+" },
+            change = { text = "~" },
+            delete = { text = "_" },
+            topdelete = { text = "‾" },
+            changedelete = { text = "~" },
+         },
+      })
+      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "[G]it [p]review hunk" })
+   end
 }
