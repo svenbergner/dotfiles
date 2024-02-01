62df0283742a2be40ee5feeabf2042b2db17877c
Sven Bergner <sven.bergner@gmail.com>
2024-02-01 11:56:34 +0100

    Only use markdown files for vimwiki

--- nvim/lua/plugins/vimwiki.lua
+++ nvim/lua/plugins/vimwiki.lua
@@ -7,9 +7,6 @@ return {
       vim.g.vimwiki_ext2syntax = { ['.md'] = 'markdown', ['.markdown'] = 'markdown', ['.mdown'] = 'markdown' }
       vim.g.vimwiki_list = {
          {
-            auto_diary_index = 1
-         },
-         {
             path = '~/Repos/vimwiki',
             syntax = 'markdown',
             ext = 'md'
@@ -50,20 +47,19 @@ return {
       -- automatically update links on read diary
       api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
          group = vimwikigroup,
-         pattern = "diary.wiki",
+         pattern = "diary.md",
          command = "VimwikiDiaryGenerateLinks",
       })
 
       -- insert diary entry header
       api.nvim_create_autocmd("BufNewFile", {
          group = vimwikigroup,
-         pattern = "*/diary/[0-9]*.(wiki|md)",
+         pattern = "*/diary/[0-9]*.md",
          command =
          'call append(0,[ "# " . strftime("%d.%m.%Y"), "", "## Tagebuch", "", "", "## Erkenntnis des Tages", "" ])',
       })
 
-      vim.keymap.set("n", "<F3>", 'i<C-R> =strftime("%A, %d.%m.%Y %I:%M")<CR><ESC>',
-         { desc = "Insert current date and time" })
-      vim.keymap.set("i", "<F3>", '<C-R> =strftime("%d.%m.%Y %I:%M")<CR>', { desc = "Insert current date and time" })
+      vim.keymap.set("n", "<F3>", ':language de_DE.UTF-8<CR>i<C-R>=strftime("%A, %d.%m.%Y %H:%M")<CR><ESC>:language en_US.UTF-8<CR>', { desc = "Insert current date and time" })
+      vim.keymap.set("i", "<F3>", '<C-R>=strftime("%d.%m.%Y %H:%M")<CR>', { desc = "Insert current date and time" })
    end
 }
