33153a867500d3ac6dab3bc49d2d641558c58009
Sven Bergner <sven.bergner@gmail.com>
2024-02-02 11:22:48 +0100

    Added second keymap to ease my brain :-)

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -1,6 +1,7 @@
 return {
    "kdheepak/lazygit.nvim",
    config = function()
-      vim.keymap.set("n", "<LEADER>gg", ":LazyGit<CR>", { silent = true, desc = "Open LazyGit overlay" })
+      vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { silent = true, desc = "Open LazyGit overlay" })
+      vim.keymap.set("n", "<leader><leader>g", ":LazyGit<CR>", { silent = true, desc = "Open LazyGit overlay" })
    end
 }
