f5a551de1531a59520915500b43756d2bb891f5a
Sven Bergner <sven.bergner@gmail.com>
2024-02-14 18:10:28 +0100

    Keymap added to toggle ZenMode

--- .config/nvim/lua/plugins/zen-mode.lua
+++ .config/nvim/lua/plugins/zen-mode.lua
@@ -7,5 +7,17 @@ return {
             height = 0.75
          }
       })
+      local toggleZenMode = function()
+         if vim.g.zen_mode_active then
+            require("zen-mode").close()
+            vim.g.zen_mode_active = false
+            vim.opt.wrap = false
+         else
+            require("zen-mode").open()
+            vim.g.zen_mode_active = true
+            vim.opt.wrap = true
+         end
+      end
+      vim.keymap.set('n', '<leader>zm', toggleZenMode, { desc = 'Toggle [Z]en[Mode]' })
    end
 }
