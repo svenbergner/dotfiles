fe68603f73c7d87537a92d9e6e1080bfc14d2ee8
Sven Bergner <sven.bergner@gmail.com>
2024-02-21 22:27:29 +0100

    Load later

--- .config/nvim/lua/plugins/eyeliner.lua
+++ .config/nvim/lua/plugins/eyeliner.lua
@@ -1,11 +1,10 @@
--- Move faster with unique f/F indicators for each word on the line. 
+-- Move faster with unique f/F indicators for each word on the line.
 -- Like quick-scope, but in Lua.
 return {
    'jinh0/eyeliner.nvim',
-   config = function()
-      require('eyeliner').setup({
-         highlight_on_key = true,
-         dim = true,
-      })
-   end
+   event = 'VeryLazy',
+   opts = {
+      highlight_on_key = true,
+      dim = true,
+   },
 }
