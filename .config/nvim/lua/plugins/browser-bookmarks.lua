c8bd90ee0d9355130e24431b16a2881191ebc7cf
Sven Bergner <sven.bergner@gmail.com>
2024-02-21 22:21:12 +0100

    Load later and comment added

--- .config/nvim/lua/plugins/browser-bookmarks.lua
+++ .config/nvim/lua/plugins/browser-bookmarks.lua
@@ -1,12 +1,13 @@
--- Telescope extension to search for a bookmark 
+-- Telescope extension to search for a bookmark
 -- and open it with the system defauult browser
 return {
-  'dhruvmanila/browser-bookmarks.nvim',
-  version = '*',
-  opts = {
-    selected_browser = 'chrome'
-  },
-  dependencies = {
-    'nvim-telescope/telescope.nvim',
-  }
+   'dhruvmanila/browser-bookmarks.nvim',
+   version = '*',
+   event = 'VeryLazy',
+   opts = {
+      selected_browser = 'chrome'
+   },
+   dependencies = {
+      'nvim-telescope/telescope.nvim',
+   }
 }
