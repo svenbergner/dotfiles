44af2f3e2d8637867534534eb428bd645f2de86f
Sven Bergner <sven.bergner@gmail.com>
2024-02-22 12:57:14 +0100

    Load later

--- .config/nvim/lua/plugins/conoline.lua
+++ .config/nvim/lua/plugins/conoline.lua
@@ -1,6 +1,7 @@
 -- Highlight current line only in active buffer
 return {
    'miyakogi/conoline.vim',
+   event = 'VeryLazy',
    config = function()
       vim.g.conoline_auto_enable = 1
       vim.g.conoline_use_colorscheme_default_normal = 1
