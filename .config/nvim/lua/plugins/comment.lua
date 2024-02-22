b07fa3d14ee24345291db214a10f67e61ff86d9f
Sven Bergner <sven.bergner@gmail.com>
2024-02-22 22:45:58 +0100

    cleanup

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,7 +1,6 @@
 -- Add or remove line or block comments
 return {
-    'numToStr/Comment.nvim',
-    opts = {},
-    lazy = false,
+   'numToStr/Comment.nvim',
+   opts = {},
+   lazy = false,
 }
-
--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,14 +1,29 @@
 return {
    'folke/which-key.nvim',
-   opts = {
-      window = {
-         border = "single",
-         margin = { 2,20,2,20 },
-         padding = { 2,5,2,5 },
-         winblend = 20,
-      },
-      layout = {
-         align = "center",
+   event = 'VeryLazy',
+   config = function()
+      require('which-key').setup({
+         window = {
+            border = "single",
+            margin = { 2, 20, 2, 20 },
+            padding = { 2, 5, 2, 5 },
+            winblend = 20,
+         },
+         layout = {
+            align = "center",
+         }
+      })
+
+      -- document existing key chains
+      require('which-key').register {
+        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
       }
-   }
+   end
 }
