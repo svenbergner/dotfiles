5ac8716d89d4fa454f9c1962815808ec94ea65e5
Sven Bergner <sven.bergner@gmail.com>
2024-02-22 22:42:22 +0100

    Remove keymap clash

--- .config/nvim/lua/plugins/vim-test.lua
+++ .config/nvim/lua/plugins/vim-test.lua
@@ -1,6 +1,7 @@
 return {
    "vim-test/vim-test",
-   config = function ()
-      vim.keymap.set("n","<leader>t", function() vim.cmd('TestNearest') end );
+   config = function()
+      vim.keymap.set("n", "<leader>tn", function() vim.cmd('TestNearest') end,
+         { desc = 'Run [t]est [n]earest' });
    end
 }
