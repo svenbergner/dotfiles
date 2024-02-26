b22317ccfc62f22ccb0bdca5e29a4059cc836760
Sven Bergner <sven.bergner@wolterskluwer.com>
2024-02-26 16:26:02 +0100

    Load lazy

--- .config/nvim/lua/plugins/actions-preview.lua
+++ .config/nvim/lua/plugins/actions-preview.lua
@@ -1,6 +1,7 @@
 -- Shows a preview of the code action before applying it
 return {
    "aznhe21/actions-preview.nvim",
+   event = 'VeryLazy',
    config = function()
       vim.keymap.set({ "v", "n" }, "ca", require("actions-preview").code_actions, { desc = "Preview [ C ]ode [ A ]ctions" })
    end
