244007c32d6693fdca64f758783a67c61c367a9f
Sven Bergner <sven.bergner@wolterskluwer.com>
2024-02-29 16:56:04 +0100

    Added a plugin for taking code screenshots

---
+++ .config/nvim/lua/plugins/nvim-silicon.lua
@@ -0,0 +1,18 @@
+-- Plugin to generate source code screenshots
+return {
+   "michaelrommel/nvim-silicon",
+   lazy = true,
+   cmd = "Silicon",
+   config = function()
+      require('silicon').setup({
+         font = "JetBrainsMono Nerd Font=34;Noto Color Emoji=34",
+         theme = "gruvbox-dark",
+         background = "#d4c4a3",
+         window_title = function()
+            return vim.fn.fnamemodify(
+               vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t"
+            )
+         end
+      })
+   end,
+}
