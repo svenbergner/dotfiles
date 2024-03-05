57eb2e5921faaaeb2d414ccefcd2cca26e3f8344
Sven Bergner <sven.bergner@wolterskluwer.com>
2024-03-05 15:44:29 +0100

    Streamlined calls

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -2,7 +2,7 @@ local api = vim.api
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
+api.nvim_create_autocmd('TextYankPost', {
    group = vim.api.nvim_create_augroup('YankHighlight', { clear = true }),
    callback = function()
       vim.highlight.on_yank()
