6e60f4c6f29280348cce1bfebac28b325b33e7e5
Sven Bergner <sven.bergner@wolterskluwer.com>
2024-03-28 13:57:59 +0100

    Missing keymappings added

--- .config/nvim/lua/plugins/vim-test.lua
+++ .config/nvim/lua/plugins/vim-test.lua
@@ -1,7 +1,14 @@
 return {
    "vim-test/vim-test",
+   dependencies = {
+      "preservim/vimux"
+   },
    config = function()
-      vim.keymap.set("n", "<leader>tn", function() vim.cmd('TestNearest') end,
-         { desc = 'Run [t]est [n]earest' });
+      vim.keymap.set("n", "<leader>t", function() vim.cmd('TestNearest') end, { desc = 'Run nearest [t]est' });
+      vim.keymap.set("n", "<leader>T", function() vim.cmd('TestFile') end, { desc = 'Run all [T]ests in file' });
+      vim.keymap.set("n", "<leader>a", function() vim.cmd('TestSuite') end, { desc = 'Run [a]ll [t]ests in suite' });
+      vim.keymap.set("n", "<leader>l", function() vim.cmd('TestLast') end, { desc = 'Run [l]ast test' });
+      vim.keymap.set("n", "<leader>g", function() vim.cmd('TestVisit') end, { desc = '[g]oto last test' });
+      vim.cmd("let test#strategy = 'vimux'")
    end
 }
