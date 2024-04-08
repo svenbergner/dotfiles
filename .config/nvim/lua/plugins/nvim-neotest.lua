67a9f2e96f815978de735ae8f405f22f5cb3dd27
Sven Bergner <sven.bergner@gmail.com>
2024-04-08 13:54:26 +0200

    Testing neotest

--- .config/nvim/lua/plugins/nvim-neotest.lua
+++ .config/nvim/lua/plugins/nvim-neotest.lua
@@ -1,11 +1,18 @@
 -- Flutter Test support
+-- See :h neotest.run.run() for parameters.
 return {
    "nvim-neotest/neotest",
    dependencies = {
-      { "sidlatau/neotest-dart" },
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "sidlatau/neotest-dart",
    },
    config = function()
-      require('neotest').setup({
+      local neotest = require('neotest')
+      neotest.setup({
          adapters = {
             require('neotest-dart') {
                command = "fvm flutter",
@@ -15,5 +22,19 @@ return {
             },
          }
       })
+      vim.keymap.set("n", "<leader>t", function() neotest.run.run() end, { desc = 'Run nearest [t]est' });
+      -- Attach to the nearest test, see :h neotest.run.attach()
+      vim.keymap.set("n", "<leader>ta", function() neotest.run.run() end, { desc = 'Nearest [t]est [a]ttach' });
+      -- Stop the nearest test, see :h neotest.run.stop()
+      vim.keymap.set("n", "<leader>ts", function() neotest.run.stop() end, { desc = 'Nearest [t]est [s]top' });
+      vim.keymap.set("n", "<leader>T", function() neotest.run.run(vim.fn.expand("%")) end,
+         { desc = 'Run all [T]ests in file' });
+      -- Debug the nearest test (requires nvim-dap and adapter support)
+      vim.keymap.set("n", "<leader>dt", function() neotest.run.run({ strategy = "dap" }) end,
+         { desc = '[d]ebug nearest [t]est' });
+      vim.keymap.set("n", "<leader>t", function() neotest.run.run() end, { desc = 'Run nearest [t]est' });
+      -- vim.keymap.set("n", "<leader>a", function()  end, { desc = 'Run [a]ll [t]ests in suite' });
+      -- vim.keymap.set("n", "<leader>l", function()  end, { desc = 'Run [l]ast test' });
+      -- vim.keymap.set("n", "<leader>g", function()  end, { desc = '[g]oto last test' });
    end,
 }
--- .config/nvim/lua/plugins/vim-test.lua
+++ .config/nvim/lua/plugins/vim-test.lua
@@ -4,11 +4,11 @@ return {
       "preservim/vimux"
    },
    config = function()
-      vim.keymap.set("n", "<leader>t", function() vim.cmd('TestNearest') end, { desc = 'Run nearest [t]est' });
-      vim.keymap.set("n", "<leader>T", function() vim.cmd('TestFile') end, { desc = 'Run all [T]ests in file' });
-      vim.keymap.set("n", "<leader>a", function() vim.cmd('TestSuite') end, { desc = 'Run [a]ll [t]ests in suite' });
-      vim.keymap.set("n", "<leader>l", function() vim.cmd('TestLast') end, { desc = 'Run [l]ast test' });
-      vim.keymap.set("n", "<leader>g", function() vim.cmd('TestVisit') end, { desc = '[g]oto last test' });
+      -- vim.keymap.set("n", "<leader>t", function() vim.cmd('TestNearest') end, { desc = 'Run nearest [t]est' });
+      -- vim.keymap.set("n", "<leader>T", function() vim.cmd('TestFile') end, { desc = 'Run all [T]ests in file' });
+      -- vim.keymap.set("n", "<leader>a", function() vim.cmd('TestSuite') end, { desc = 'Run [a]ll [t]ests in suite' });
+      -- vim.keymap.set("n", "<leader>l", function() vim.cmd('TestLast') end, { desc = 'Run [l]ast test' });
+      -- vim.keymap.set("n", "<leader>g", function() vim.cmd('TestVisit') end, { desc = '[g]oto last test' });
       vim.cmd("let test#strategy = 'vimux'")
    end
 }
