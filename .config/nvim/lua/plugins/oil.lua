85870f4762f39d5ac980f9ce84be2778417d6398
Sven Bergner <sven.bergner@gmail.com>
2024-04-10 09:22:04 +0200

    Additional filemanager plugin added

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,13 @@
+return {
+   "stevearc/oil.nvim",
+   opts = {},
+   dependencies = {
+      "nvim-tree/nvim-web-devicons",
+   },
+   config = function ()
+      require("oil").setup({
+         default_file_explorer = false,
+      })
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+   end
+}
