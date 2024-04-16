354b056f2d18b650c1d2658e4fdb2a5839aa947d
Sven Bergner <sven.bergner@gmail.com>
2024-04-16 18:10:57 +0200

    Fixed some colors

--- .config/wezterm/utils/tabs_format.lua
+++ .config/wezterm/utils/tabs_format.lua
@@ -21,9 +21,9 @@ local function tab_title(tab_info)
   return tab_info.active_pane.title
 end
 
-wezterm.on( 'format-tab-title',
+wezterm.on('format-tab-title',
   function(tab, tabs, panes, config, hover, max_width)
-    local edge_background = '#0b0022'
+    local edge_background = '#3c3836'
     local background = '#1b1032'
     local foreground = '#808080'
 
@@ -44,13 +44,15 @@ wezterm.on( 'format-tab-title',
     title = wezterm.truncate_right(title, max_width - 2)
 
     return {
-      { Text = " " },
       { Background = { Color = edge_background } },
+      { Text = " " },
       { Foreground = { Color = background } },
       { Text = SOLID_LEFT_ARROW },
       { Background = { Color = background } },
       { Foreground = { Color = foreground } },
-      { Text = tab.tab_index+1 .. " " },
+      { Attribute = { Intensity = "Bold" } },
+      { Text = tab.tab_index + 1 .. " " },
+      { Attribute = { Intensity = "Normal" } },
       { Background = { Color = edge_background } },
       { Foreground = { Color = edge_foreground } },
       { Text = " " .. title },
@@ -59,3 +61,21 @@ wezterm.on( 'format-tab-title',
 )
 
 return {}
+
+-- scheme: "Gruvbox dark, hard"
+-- base00: "#1d2021" # ----
+-- base01: "#3c3836" # ---
+-- base02: "#504945" # --
+-- base03: "#665c54" # -
+-- base04: "#bdae93" # +
+-- base05: "#d5c4a1" # ++
+-- base06: "#ebdbb2" # +++
+-- base07: "#fbf1c7" # ++++
+-- base08: "#fb4934" # red
+-- base09: "#fe8019" # orange
+-- base0A: "#fabd2f" # yellow
+-- base0B: "#b8bb26" # green
+-- base0C: "#8ec07c" # aqua/cyan
+-- base0D: "#83a598" # blue
+-- base0E: "#d3869b" # purple
+-- base0F: "#d65d0e" # brown
