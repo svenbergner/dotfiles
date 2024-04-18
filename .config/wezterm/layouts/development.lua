ec4e7f1abd41a944051c0b40647a41881373dcd5
Sven Bergner <sven.bergner@gmail.com>
2024-04-18 22:21:17 +0200

    Full layout added but it's not working

--- .config/wezterm/layouts/development.lua
+++ .config/wezterm/layouts/development.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local mux = wezterm.mux
+local config = {}
 
 wezterm.on('gui-startup', function(cmd)
   local args = {}
@@ -8,7 +9,7 @@ wezterm.on('gui-startup', function(cmd)
   end
 
   local home_dir = wezterm.home_dir
-  local _, _, window = mux.spawn_window {
+  local tab, pane, window = mux.spawn_window {
     workspace = 'Development',
     cwd = home_dir,
     args = args,
@@ -23,22 +24,32 @@ wezterm.on('gui-startup', function(cmd)
   }
   second_tab:set_title('SimplyTaxApp')
 
+  local third_tab, _, _ = window:mux_window():spawn_tab {
+    cwd = home_dir .. '/Repos/SSE/Dev',
+  }
+  third_tab:set_title( 'SSE/TaxCoreApi' )
 
-  -- _, third_pane, _ = window:spawn_tab {
-  --   workspace = 'SimplyTaxApp',
-  --   cwd = home_dir .. '/Repos/simply_tax_app',
-  --   args = args,
-  -- }
-  -- tab, build_pane, window = mux.spawn_window {
-  --   workspace = 'SSE/TaxCoreApi',
-  --   cwd = home_dir .. '/Repos/SSE/Dev',
-  --   args = args,
-  -- }
-  --
+  local fourth_tab, _, _ = window:mux_window():spawn_tab {
+    cwd = home_dir .. '/Repos/webhook-builds',
+  }
+  fourth_tab:set_title( 'Build-Pipelines' )
+
+  local fifth_tab, _, _ = window:mux_window():spawn_tab {
+    cwd = home_dir .. '/Repos/vimwiki',
+  }
+  fifth_tab:set_title( 'VimWiki' )
+
+  local sixth_tab, _, _ = window:mux_window():spawn_tab {
+    cwd = home_dir,
+  }
+  sixth_tab:set_title( 'Shell' )
+
   -- local editor_pane = build_pane:split {
   --   direction = 'Top',
   --   size = 0.6,
   --   cwd = project_dir,
   -- }
 end)
+
+return config
 
