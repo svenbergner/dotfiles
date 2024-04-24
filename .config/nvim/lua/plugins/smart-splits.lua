c4084d5752d7ecc6c9fed551470b6f0822f0702e
Sven Bergner <sven.bergner@gmail.com>
2024-04-24 11:03:57 +0200

    Added the smart splits plugin

---
+++ .config/nvim/lua/plugins/smart-splits.lua
@@ -0,0 +1,32 @@
+-- Smarter and more intuitive split pane management that uses a mental model of
+-- left/right/up/down instead of wider/narrower/taller/shorter for resizing.
+-- Supports seamless navigation between Neovim and terminal multiplexer split panes.
+return
+{
+   'mrjones2014/smart-splits.nvim',
+   config = function()
+      -- recommended mappings
+      -- resizing splits
+      -- these keymaps will also accept a range,
+      -- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
+      local smartsplits = require('smart-splits')
+
+      vim.keymap.set('n', '<A-h>', smartsplits.resize_left, { desc = "Change width of pane" })
+      vim.keymap.set('n', '<A-j>', smartsplits.resize_down, { desc = "Change height of pane" } )
+      vim.keymap.set('n', '<A-k>', smartsplits.resize_up, { desc = "Change height of pane" } )
+      vim.keymap.set('n', '<A-l>', smartsplits.resize_right, { desc = "Change width of pane" } )
+
+      -- moving between splits
+      vim.keymap.set('n', '<C-h>', smartsplits.move_cursor_left, { desc = "Move focus to the left pane" } )
+      vim.keymap.set('n', '<C-j>', smartsplits.move_cursor_down, { desc = "Move focus to the bottom pane" } )
+      vim.keymap.set('n', '<C-k>', smartsplits.move_cursor_up, { desc = "Move focus to the top pane" } )
+      vim.keymap.set('n', '<C-l>', smartsplits.move_cursor_right, { desc = "Move focus to the right pane" } )
+      vim.keymap.set('n', '<C-\\>', smartsplits.move_cursor_previous, { desc = "Move focus to the previous pane" } )
+
+      -- swapping buffers between windows
+      vim.keymap.set('n', '<leader><leader>h', smartsplits.swap_buf_left, { desc = "Swap current pane with the left" } )
+      vim.keymap.set('n', '<leader><leader>j', smartsplits.swap_buf_down, { desc = "Swap current pane with the bottom" } )
+      vim.keymap.set('n', '<leader><leader>k', smartsplits.swap_buf_up, { desc = "Swap current pane with the top" } )
+      vim.keymap.set('n', '<leader><leader>l', smartsplits.swap_buf_right, { desc = "Swap current pane with the right" } )
+   end
+}
