f11c9a6335e12001770ac558d83e7be384ef217e
Sven Bergner <sven.bergner@gmail.com>
2024-04-28 22:43:30 +0200

    Starting nvim removed

--- .config/wezterm/layouts/development.lua
+++ .config/wezterm/layouts/development.lua
@@ -3,29 +3,24 @@ local M = {}
 M.setup_layout = function(wezterm)
   local mux = wezterm.mux
   local act = wezterm.action
-  wezterm.on('gui-startup', function(cmd)
-    local args = {}
-    if cmd then
-      args = cmd.args
-    end
-
+  wezterm.on('gui-startup', function()
     local home_dir = wezterm.home_dir
     local first_tab, _, window = mux.spawn_window {
       workspace = 'Development',
       cwd = home_dir .. '/Repos/simply_tax_app',
-      args = { '/usr/local/bin/nvim', '.' },
+      -- args = { '/usr/local/bin/nvim', '.' },
     }
     first_tab:set_title('SimplyTaxApp')
 
     local second_tab, _, _ = window:spawn_tab {
       cwd = home_dir .. '/Repos/SSE/Dev',
-      args = { '/usr/local/bin/nvim', '.' },
+      -- args = { '/usr/local/bin/nvim', '.' },
     }
     second_tab:set_title('SSE/TaxCoreApi')
 
     local third_tab, _, _ = window:spawn_tab {
       cwd = home_dir .. '/Repos/webhook-builds',
-      args = { '/usr/local/bin/nvim', '.' },
+      -- args = { '/usr/local/bin/nvim', '.' },
     }
     third_tab:set_title('Build-Pipelines')
 
