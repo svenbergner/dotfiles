0f1c006091e068f237625fbd294589cff11322bc
Sven Bergner <sven.bergner@gmail.com>
2024-04-29 16:53:35 +0200

    Extracted pretty screenshot settings

---
+++ .config/wezterm/utils/pretty_screenshots.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+M.apply_to_config = function(config)
+  config.enable_tab_bar = false
+  config.window_padding = {
+    left = '0.5cell',
+    right = '0.5cell',
+    top = '0.5cell',
+    bottom = '0cell',
+  }
+end
+
+return M
