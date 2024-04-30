3892031d9ac5b5a79c438fe00b30e157c52fb5ac
Sven Bergner <sven.bergner@wolterskluwer.com>
2024-04-30 17:49:50 +0200

    Pimp up my telescope settings

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,9 @@ return {
    {
       "nvim-telescope/telescope.nvim",
       dependencies = {
+         "ThePrimeagen/harpoon",
          "nvim-lua/plenary.nvim",
+         "joshmedeski/telescope-smart-goto.nvim",
       },
       config = function()
          local builtin = require("telescope.builtin")
@@ -30,6 +32,15 @@ return {
             builtin.buffers { attach_mappings = mapping }
          end, { desc = "[f]ind [b]uffers" })
 
+         vim.keymap.set("n", "<leader>/", function()
+            -- You can pass additional configuration to telescope to change theme, layout, etc.
+            require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+               winblend = 10,
+               previewer = false,
+               layout_config = { width = 0.7 },
+            }))
+         end, { desc = "[/] Fuzzily search in current buffer" })
+
          vim.keymap.set("n", "<leader>fi", '<cmd>AdvancedGitSearch<cr>', { desc = "Advanced Git Search" })
          vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Find Help Tags" })
          vim.keymap.set('n', '<F12>', builtin.help_tags, { desc = "Find Help Tags" })
@@ -41,8 +52,6 @@ return {
          vim.keymap.set('n', '<leader>Gb', builtin.git_branches, { desc = '[G]it [b]ranches' })
          vim.keymap.set('n', '<leader>sj', builtin.jumplist, { desc = "[S]how [J]umplist" })
          vim.keymap.set('n', '<leader>df', require('telescope.builtin').filetypes, { desc = '[D]ocument [f]iletype' })
-
-         pcall(require('telescope').load_extension, 'fzf')
       end,
    },
    {
@@ -54,8 +63,62 @@ return {
       'dawsers/telescope-floaterm.nvim',
       'LukasPietzschmann/telescope-tabs',
       "svenbergner/telescope-debugee-selector",
+
       config = function()
+         local telescope = require("telescope")
+         local telescopeConfig = require("telescope.config")
+
+         -- Clone the default Telescope configuration
+         local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+         -- I want to search in hidden/dot files.
+         table.insert(vimgrep_arguments, "--hidden")
+         -- I don't want to search in the `.git` directory.
+         table.insert(vimgrep_arguments, "--glob")
+         table.insert(vimgrep_arguments, "!**/.git/*")
+
+         local actions = require("telescope.actions")
+
+         local select_one_or_multi = function(prompt_bufnr)
+            local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+            local multi = picker:get_multi_selection()
+            if not vim.tbl_isempty(multi) then
+               require("telescope.actions").close(prompt_bufnr)
+               for _, j in pairs(multi) do
+                  if j.path ~= nil then
+                     vim.cmd(string.format("%s %s", "edit", j.path))
+                  end
+               end
+            else
+               actions.select_default(prompt_bufnr)
+            end
+         end
          require("telescope").setup({
+            defaults = {
+               -- `hidden = true` is not supported in text grep commands.
+               vimgrep_arguments = vimgrep_arguments,
+               path_display = { "truncate" },
+               mappings = {
+                  n = {
+                     ["<C-w>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                  },
+                  i = {
+                     ["<C-j>"] = actions.cycle_history_next,
+                     ["<C-k>"] = actions.cycle_history_prev,
+                     ["<CR>"] = select_one_or_multi,
+                     ["<C-w>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                     ["<C-S-d>"] = actions.delete_buffer,
+                     ["<C-s>"] = actions.cycle_previewers_next,
+                     ["<C-a>"] = actions.cycle_previewers_prev,
+                  },
+               },
+            },
+            pickers = {
+               find_files = {
+                  -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+                  find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+               },
+            },
             extensions = {
                ["ui-select"] = {
                   require("telescope.themes").get_dropdown({}),
@@ -80,19 +143,24 @@ return {
                ['debugee_selector'] = {},
             },
          })
-         require("telescope").load_extension("ui-select")
-         require("telescope").load_extension("flutter")
-         require("telescope").load_extension("noice")
+
 
          require("telescope").load_extension("neoclip")
 
+         require("telescope").load_extension("fzf")
+
+         require("telescope").load_extension("ui-select")
          vim.g.zoxide_use_select = true
+
+         require("telescope").load_extension("flutter")
 
          require("telescope").load_extension("undo")
          require("telescope").load_extension("live_grep_args")
          require("telescope").load_extension("floaterm")
          require("telescope").load_extension("repo")
          require("telescope").load_extension("debugee_selector")
+         require("telescope").load_extension("noice")
+         require("telescope").load_extension("advanced_git_search")
       end,
    },
 }
