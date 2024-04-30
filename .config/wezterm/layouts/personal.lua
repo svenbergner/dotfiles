8e998e0c970b9159af9fc59d672f9de50484f52b
Sven Bergner <sven.bergner@gmail.com>
2024-04-30 11:45:12 +0200

    Start nvim in the right directory in an actual shell

--- .config/wezterm/layouts/personal.lua
+++ .config/wezterm/layouts/personal.lua
@@ -6,18 +6,18 @@ M.setup_layout = function(wezterm)
 
   wezterm.on('gui-startup', function()
     local home_dir = wezterm.home_dir
-    local first_tab, _, window = mux.spawn_window {
+    local first_tab, first_pane, window = mux.spawn_window {
       workspace = 'Personal',
       cwd = home_dir .. '/Repos/dotfiles/.config/',
-      -- args = { os.getenv('SHELL'), '-c', 'nvim ' .. wezterm.shell_quote_arg( home_dir .. '/Repos/dotfiles/.config/'  )},
     }
     first_tab:set_title('Dotfiles')
+    first_pane:send_text('nvim .\n')
 
-    local second_tab, _, _ = window:spawn_tab {
+    local second_tab, second_pane, _ = window:spawn_tab {
       cwd = home_dir .. '/Repos/vimwiki',
-      -- args = { '/usr/local/bin/nvim', home_dir .. '/Repos/vimwiki' },
     }
     second_tab:set_title('VimWiki')
+    second_pane:send_text('nvim .\n')
 
     act.ActivateTab(0)
   end)
