35f83ab79bc6ce73008b858e7455377a0801b3ed
Sven Bergner <sven.bergner@gmail.com>
2024-05-02 09:35:02 +0200

    lsp warnings behoben

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -47,7 +47,6 @@ return {
          -- This module contains a number of default definitions
          local rainbow_delimiters = require 'rainbow-delimiters'
 
-         ---@type rainbow_delimiters.config
          vim.g.rainbow_delimiters = {
             strategy = {
                [''] = rainbow_delimiters.strategy['global'],
