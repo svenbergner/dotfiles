5be3b11bed119b95ca2a5ef3fa239398f2efa131
Sven Bergner <sven.bergner@gmail.com>
2024-05-07 08:59:35 +0200

    Changed description to match keymapping

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -14,6 +14,7 @@ return {
          },
          numhl = true, -- Change color of line number
       })
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "[G]it [p]review hunk" })
+      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "[g]it [p]review hunk" })
+      vim.keymap.set("n", "<leader>gs", ":Gitsigns stage_hunk<CR>", { desc = "[g]it [s]tage hunk" })
    end
 }
