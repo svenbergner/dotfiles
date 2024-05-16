5fbdb37e7cd87ea510d8b55fef46c5cf73956140
Sven Bergner <sven.bergner@gmail.com>
2024-05-16 13:51:31 +0200

    Added keymap for opening an floating window

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -6,10 +6,18 @@ return {
    dependencies = {
       "nvim-tree/nvim-web-devicons",
    },
-   config = function ()
+   config = function()
       require("oil").setup({
          default_file_explorer = false,
+         columns = {
+            "icon",
+         }
       })
+      -- Open parent directory in current window
       vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+      -- Open parent directory in floating window
+      vim.keymap.set("n", "<leader>-", require('oil').toggle_float,
+         { desc = "Open parent directory in a floating window" })
    end
 }
