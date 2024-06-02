f2276b75963899de47fda953ea6b89240cfac0a4
Sven Bergner <sven.bergner@gmail.com>
2024-06-02 20:12:08 +0200

    Comment more precise

--- .config/nvim/ftdetect/aavdrm.lua
+++ .config/nvim/ftdetect/aavdrm.lua
@@ -1,4 +1,4 @@
--- Detect aavdrm and interpret it as dosini file
+-- Detect aavdrm and interpret it as DOS Ini-file to have syntax highlighting
 
 vim.api.nvim_create_autocmd(
    {'BufRead','BufNewFile'},
