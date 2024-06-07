24643ea8fd6eb22012235381603dd36c56ae60d5
Sven Bergner <sven.bergner@gmail.com>
2024-06-07 14:41:42 +0200

    Solve performance problems with github copilot

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -23,6 +23,13 @@ return {
       local cmp = require("cmp")
       require("luasnip.loaders.from_vscode").lazy_load()
       local luasnip = require("luasnip")
+
+      local has_words_before = function()
+         if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+         return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+      end
+
       luasnip.config.setup({})
       cmp.setup({
          formatting = {
@@ -31,6 +38,7 @@ return {
                maxwidth = 120,
                ellipsis_char = '...',
                show_labelDetails = true,
+               symbol_map = { Copilot = "" },
             })
          },
          snippet = {
@@ -51,7 +59,7 @@ return {
                select = false,
             }),
             ["<Tab>"] = cmp.mapping(function(fallback)
-               if cmp.visible() then
+               if cmp.visible() and has_words_before() then
                   cmp.select_next_item()
                elseif luasnip.expand_or_jumpable() then
                   luasnip.expand_or_jump()
@@ -69,7 +77,25 @@ return {
                end
             end, { "i", "s" }),
          }),
+         sorting = {
+            priority_weight = 2,
+            comparators = {
+               require("copilot_cmp.comparators").prioritize,
+               -- Below is the default comparitor list and order for nvim-cmp
+               cmp.config.compare.offset,
+               -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+               cmp.config.compare.exact,
+               cmp.config.compare.score,
+               cmp.config.compare.recently_used,
+               cmp.config.compare.locality,
+               cmp.config.compare.kind,
+               cmp.config.compare.sort_text,
+               cmp.config.compare.length,
+               cmp.config.compare.order,
+            }
+         },
          sources = {
+            { name = "copilot" },
             { name = "nvim_lua" },
             { name = "nvim_lsp" },
             { name = "path" },
@@ -81,7 +107,9 @@ return {
       cmp.setup.cmdline('/', {
          mapping = cmp.mapping.preset.cmdline(),
          sources = {
-            { name = 'buffer' }
+            { name = 'buffer' },
+            { name = 'path' },
+            { name = 'file' }
          }
       })
 
