9a544fef53cb6907c5a2523a82e4e271fe770805
Sven Bergner <sven.bergner@wolterskluwer.com>
2024-06-11 09:26:24 +0200

    Copilot Chat added

--- .config/nvim/lua/plugins/github-copilot.lua
+++ .config/nvim/lua/plugins/github-copilot.lua
@@ -1,6 +1,6 @@
 return {
-   -- "github/copilot.vim"
-   { 
+   -- -- "github/copilot.vim"
+   {
       "zbirenbaum/copilot.lua",
       config = function()
          require("copilot").setup({
@@ -14,5 +14,18 @@ return {
       config = function()
          require("copilot_cmp").setup()
       end
-   }
+   },
+   {
+      "CopilotC-Nvim/CopilotChat.nvim",
+      branch = "canary",
+      dependencies = {
+         { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+         { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+      },
+      opts = {
+         debug = true, -- Enable debugging
+         -- See Configuration section for rest
+      },
+      -- See Commands section for default commands if you want to lazy load on them
+   },
 }
