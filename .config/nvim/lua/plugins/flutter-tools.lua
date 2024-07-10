5eebb06eeded60b7bb6d4b362cd77fd813b52378
Sven Bergner <sven.bergner@DE64LRL006QCG.fritz.box>
2024-07-10 11:29:07 +0200

    Load lazy for filetype dart

--- .config/nvim/lua/plugins/flutter-tools.lua
+++ .config/nvim/lua/plugins/flutter-tools.lua
@@ -1,7 +1,8 @@
 -- Plugin which adds some tools for developing with flutter
 return {
    "akinsho/flutter-tools.nvim",
-   lazy = false,
+   lazy = true,
+   ft = { "dart" },
    dependencies = {
       "nvim-lua/plenary.nvim",
       "stevearc/dressing.nvim", -- optional for vim.ui.select
