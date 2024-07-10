c8867ead7d834791436faf92ae14a823627ee370
Sven Bergner <sven.bergner@DE64LRL006QCG.fritz.box>
2024-07-10 11:29:48 +0200

    Use stop sign for breakpoints and colors tweaked

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -24,7 +24,7 @@ return {
          type = "server",
          port = "${port}",
          executable = {
-            command = '/Users/sven.bergner/.local/share/nvim/mason/bin/codelldb',--vim.fn.exepath("codelldb"),
+            command = '/Users/sven.bergner/.local/share/nvim/mason/bin/codelldb', --vim.fn.exepath("codelldb"),
             args = { "--port", "${port}" },
          },
       }
@@ -61,8 +61,8 @@ return {
             name = "Launch Dart Program",
             program = "${file}",
             cwd = "${workspaceFolder}",
-            args = { "--dart-define-from-file=emv-vars.json"}, -- Note for Dart Apps this is args, for Flutter apps toolArgs
-            toolsArgs = { "--dart-define-from-file=emv-vars.json"}, -- Note for Dart Apps this is args, for Flutter apps toolArgs
+            args = { "--dart-define-from-file=emv-vars.json" },      -- Note for Dart Apps this is args, for Flutter apps toolArgs
+            toolsArgs = { "--dart-define-from-file=emv-vars.json" }, -- Note for Dart Apps this is args, for Flutter apps toolArgs
          }
       }
 
@@ -95,6 +95,7 @@ return {
       vim.api.nvim_create_user_command("SetDebuggee", function()
          require('telescope').extensions.debugee_selector.show_debugee_candidates()
       end, { nargs = 0 })
+
       vim.api.nvim_create_user_command("ResetDebuggee", function()
          dap.configurations.cpp = {
             {
@@ -110,19 +111,47 @@ return {
             },
          }
       end, { nargs = 0 })
+
       vim.api.nvim_create_user_command("ShowDebuggee", function()
          vim.print("Current debuggee: ", dap.configurations.cpp[1].program)
       end, { nargs = 0 })
+
       vim.api.nvim_create_user_command("DapLoadLldbForCpp", function()
          require("dap.ext.vscode").load_launchjs(vim.fn.getcwd() .. "/.vscode/launch.json", { lldb = { "cpp" } })
       end, { nargs = 0 })
+
       -- dap.adapters.python = {
       --     type = "executable",
       --     command = vim.fn.expand("~/") .. "/.local/share/nvim/mason/packages/debugpy/venv/bin/python",
       --     args = { "-m", "debugpy.adapter" },
       -- }
 
-      vim.fn.sign_define("DapStopped", { text = "▶️", texthl = "", linehl = "", numhl = "" })
+      vim.api.nvim_create_autocmd("ColorScheme", {
+         pattern = "*",
+         desc = "Prevent colorscheme clearing self-defined DAP marker colors",
+         callback = function()
+            -- Reuse current SignColumn background (except for DapStoppedLine)
+            local sign_column_hl = vim.api.nvim_get_hl(0, { name = 'SignColumn' })
+            -- if bg or ctermbg aren't found, use bg = 'bg' (which means current Normal) and ctermbg = 'Black'
+            -- convert to 6 digit hex value starting with #
+            local sign_column_bg = (sign_column_hl.bg ~= nil) and ('#%06x'):format(sign_column_hl.bg) or 'bg'
+            local sign_column_ctermbg = (sign_column_hl.ctermbg ~= nil) and sign_column_hl.ctermbg or 'Black'
+
+            vim.api.nvim_set_hl(0, 'DapStopped', { fg = '#98c379', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+            vim.api.nvim_set_hl(0, 'DapStoppedLine', { bg = '#2e4d3d', ctermbg = 'Green' })
+            vim.api.nvim_set_hl(0, 'DapBreakpoint',
+               { fg = '#993939', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+            vim.api.nvim_set_hl(0, 'DapBreakpointRejected',
+               { fg = '#888ca6', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+            vim.api.nvim_set_hl(0, 'DapLogPoint', { fg = '#61afef', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+         end
+      })
+
+      -- reload current color scheme to pick up colors override if it was set up in a lazy plugin definition fashion
+      vim.cmd.colorscheme(vim.g.colors_name)
+
+      vim.fn.sign_define("DapBreakpoint", { text = "🛑", numhl = "DapBreakpoint" })
+      vim.fn.sign_define("DapStopped", { text = "→", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
 
       -- vscode-style debugging
       vim.keymap.set("n", "<F5>", dap.continue, { desc = 'Start/Continue Debugging' })
