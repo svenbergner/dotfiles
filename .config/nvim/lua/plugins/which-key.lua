ebe9e17ee35dad8d7ab68aafec884d3e72acbcf2
Sven Bergner <sven.bergner@gmail.com>
2024-07-20 23:47:51 +0200

    Fixed warnings

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -2,9 +2,13 @@
 return {
    'folke/which-key.nvim',
    event = 'VeryLazy',
+   dependencies = {
+      { 'echasnovski/mini.icons', version = false },
+   },
    config = function()
       require('which-key').setup({
-         window = {
+         win = {
+            no_overlap = true,
             border = "single",
             margin = { 2, 20, 2, 20 },
             padding = { 2, 5, 2, 5 },
@@ -12,19 +16,26 @@ return {
          },
          layout = {
             align = "center",
+         },
+         -- document existing key chains
+         keys = {
+            { "<leader>c",  group = "[C]ode" },
+            { "<leader>c_", hidden = true },
+            { "<leader>d",  group = "[D]ocument" },
+            { "<leader>d_", hidden = true },
+            { "<leader>g",  group = "[G]it" },
+            { "<leader>g_", hidden = true },
+            { "<leader>h",  group = "Git [H]unk" },
+            { "<leader>h_", hidden = true },
+            { "<leader>r",  group = "[R]ename" },
+            { "<leader>r_", hidden = true },
+            { "<leader>s",  group = "[S]earch" },
+            { "<leader>s_", hidden = true },
+            { "<leader>t",  group = "[T]oggle" },
+            { "<leader>t_", hidden = true },
+            { "<leader>w",  group = "[W]orkspace" },
+            { "<leader>w_", hidden = true },
          }
       })
-
-      -- document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      }
    end
 }
