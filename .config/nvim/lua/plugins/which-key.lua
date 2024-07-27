375fd8a9ce2e3d2558b5e4dfb8ad9627692946fb
Sven Bergner <sven.bergner@DE64LRL006QCG.fritz.box>
2024-07-27 19:13:35 +0200

    Unneed keymaps removed

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -19,25 +19,6 @@ return {
          layout = {
             align = "center",
          },
-         -- document existing key chains
-         keys = {
-            { "<leader>c",  group = "[C]ode" },
-            { "<leader>c_", hidden = true },
-            { "<leader>d",  group = "[D]ocument" },
-            { "<leader>d_", hidden = true },
-            { "<leader>g",  group = "[G]it" },
-            { "<leader>g_", hidden = true },
-            { "<leader>h",  group = "Git [H]unk" },
-            { "<leader>h_", hidden = true },
-            { "<leader>r",  group = "[R]ename" },
-            { "<leader>r_", hidden = true },
-            { "<leader>s",  group = "[S]earch" },
-            { "<leader>s_", hidden = true },
-            { "<leader>t",  group = "[T]oggle" },
-            { "<leader>t_", hidden = true },
-            { "<leader>w",  group = "[W]orkspace" },
-            { "<leader>w_", hidden = true },
-         },
          show_help = true,
          show_keys = true,
          disable = {
