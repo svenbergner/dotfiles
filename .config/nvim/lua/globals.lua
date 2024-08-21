908a0910448c5c14f6219742402aa02775caf4f2
Sven Bergner <sven.bergner@gmail.com>
2024-08-21 14:21:21 +0200

    Moved all globals into seperate file

---
+++ .config/nvim/lua/globals.lua
@@ -0,0 +1,13 @@
+-- Map leader and localleader to space
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Backspace deletes indent, end of line and
+vim.g.backspace = "indent,eol,start"
+
+vim.g.noerrorbells = true
+vim.g.nohlsearch = true
+vim.g.icm = "nosplit"
+
+-- No perl provider needed
+vim.g.loaded_perl_provider = 0
