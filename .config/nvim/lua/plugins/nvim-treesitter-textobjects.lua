8304b5d294371a2f16a3dd2375f7c77028caeb83
Sven Bergner <sven.bergner@wolterskluwer.com>
2024-08-23 14:21:20 +0200

    Added capture groups

--- .config/nvim/lua/plugins/nvim-treesitter-textobjects.lua
+++ .config/nvim/lua/plugins/nvim-treesitter-textobjects.lua
@@ -4,22 +4,22 @@ return {
    lazy = true,
    config = function()
       require("nvim-treesitter.configs").setup({
-         --       textobjects = {
-         --          select = {
-         --             enable = true,
-         --
-         --             -- Automatically jump forward to textobj, similar to targets.vim
-         --             lookahead = true,
-         --
-         --             keymaps = {
-         --                -- You can use capture groups defined in textobjects.scm
-         --                ["a="] = { "@parameter.outer", description = "Select outer part of an assignment" },
-         --                ["i="] = { "@parameter.inner", description = "Select inner part of an assignment" },
-         --                ["l="] = { "@parameter.lhs", description = "Select left hand side of an assignment" },
-         --                ["r="] = { "@parameter.rhs", description = "Select right hand side of an assignment" },
-         --             }
-         --          }
-         --       }
+         textobjects = {
+            select = {
+               enable = true,
+
+               -- Automatically jump forward to textobj, similar to targets.vim
+               lookahead = true,
+
+               keymaps = {
+                  -- You can use capture groups defined in textobjects.scm
+                  ["a="] = { query = "@parameter.outer", description = "Select outer part of an assignment" },
+                  ["i="] = { query = "@parameter.inner", description = "Select inner part of an assignment" },
+                  ["l="] = { query = "@parameter.lhs", description = "Select left hand side of an assignment" },
+                  ["r="] = { query = "@parameter.rhs", description = "Select right hand side of an assignment" },
+               }
+            }
+         }
       })
    end
 }
