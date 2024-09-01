b4394913ee623a9d3274ef1c184f53103ddfa2f9
Sven Bergner <sven.bergner@gmail.com>
2024-09-01 10:56:19 +0200

    Added more descriptions

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -41,6 +41,8 @@ return {
                end,
                cwd = "${workspaceFolder}",
                stopOnEntry = false,
+               -- args = { "-update" },
+               -- args = { "-c", "-mnormal" },
                args = {},
                initCommands = function()
                   local commands = {}
@@ -205,14 +207,14 @@ return {
    {
       "theHamsta/nvim-dap-virtual-text",
       opts = {
-         enabled_commands = true,       -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+         enabled_commands = true,            -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
          highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-         highlight_new_as_changed = true, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-         show_stop_reason = true,       -- show stop reason when stopped for exceptions
-         commented = true,             -- prefix virtual text with comment string
-         only_first_definition = false,  -- only show virtual text at first definition (if there are multiple)
-         all_references = true,        -- show virtual text on all all references of the variable (not only definitions)
-         clear_on_continue = false,     -- clear virtual text on "continue" (might cause flickering when stepping)
+         highlight_new_as_changed = true,    -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+         show_stop_reason = true,            -- show stop reason when stopped for exceptions
+         commented = true,                   -- prefix virtual text with comment string
+         only_first_definition = false,      -- only show virtual text at first definition (if there are multiple)
+         all_references = true,              -- show virtual text on all all references of the variable (not only definitions)
+         clear_on_continue = false,          -- clear virtual text on "continue" (might cause flickering when stepping)
          virt_text_win_col = 120,
          virt_text_pos = "inline",
       }
