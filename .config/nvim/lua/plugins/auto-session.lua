849ef0ea764add4b80941f9665fa8ec8a625e1c5
Sven Bergner <sven.bergner@gmail.com>
2024-09-01 10:57:10 +0200

    cleanup

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -4,13 +4,13 @@ return {
    config = function()
       require('auto-session').setup(
          {
-            auto_session_suppress_dirs = { "~/", "~/Download", "/", },
             session_lens = {
                buftypes_to_ignore = {},
                load_on_setup = true,
-               theme_conf = { border = true },
                previewer = false,
+               theme_conf = { border = true },
             },
+            suppress_dirs = { "~/", "~/Download", "/", },
             vim.keymap.set('n', '<leader>ls',
                require('auto-session.session-lens').search_session,
                { noremap = true })
