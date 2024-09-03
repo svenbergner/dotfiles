a33d821bfc9b3b7fb6417ad17f0367088bb794ad
Sven Bergner <sven.bergner@wolterskluwer.com>
2024-09-03 21:04:47 +0200

    Comments removed

--- .config/nvim/lua/plugins/barbar.lua
+++ .config/nvim/lua/plugins/barbar.lua
@@ -12,8 +12,8 @@ return {
    {
       'romgrk/barbar.nvim',
       dependencies = {
-         'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
-         'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+         'lewis6991/gitsigns.nvim',
+         'nvim-tree/nvim-web-devicons',
       },
       init = function()
          vim.g.barbar_auto_setup = false
