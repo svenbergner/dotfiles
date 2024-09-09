8f264266b22211d335d782d08fdfe6440c43e2bc
Sven Bergner <sven.bergner@gmail.com>
2024-09-09 16:29:12 +0200

    More entries added

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -1,13 +1,29 @@
 -- Highlight todo, notes, etc in comments
 -- TODO: 
+-- OPTIMIZE:
+-- PERFORMANCE:
+-- TEST:
+-- PROJECT:
 -- FIXME:
+-- BUG:
 -- WARN:
+-- HACK:
 -- NOTE:
---
+-- INFO:
+
 return {
    'folke/todo-comments.nvim',
    dependencies = {
       'nvim-lua/plenary.nvim'
    },
-   opts = { signs = true },
+   opts = {
+      signs = true,
+      keywords = {
+         FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
+         PROJECT = { icon = "  ", color = "info", alt = { "PROJECT" } },
+         TODO = { icon = " ", color = "info", alt = { "TODO" } },
+         WARN = { icon = " ", color = "warning", alt = { "WARN", "WARNING" } },
+         NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+      },
+   },
 }
