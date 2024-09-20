caea3d663778bff3ecd547f989637ffabd005102
Sven Bergner <sven.bergner@wolterskluwer.com>
2024-09-20 17:17:16 +0200

    Disable default provider

--- .config/nvim/lua/globals.lua
+++ .config/nvim/lua/globals.lua
@@ -9,5 +9,8 @@ vim.g.backspace = "indent,eol,start"
 vim.g.noerrorbells = true
 vim.g.nohlsearch = true
 
--- No perl provider needed
+-- disable some default providers
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python3_provider = 0
 vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
