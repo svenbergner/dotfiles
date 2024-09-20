c1e813bb8149ca7802a20a40234108fcd82eb846
Sven Bergner <sven.bergner@gmail.com>
2024-09-20 15:48:46 +0200

    Linelength

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -51,7 +51,8 @@ return {
                   table.insert(commands, "breakpoint name configure --disable cpp_exception")
                   if vim.fn.has("mac") == 1 then
                      table.insert(commands,
-                        'script -l python -- import sys; sys.path += [ "' .. vim.fn.expand("$HOME") .. '/Library/Python/3.9/lib/python/site-packages" ]')
+                        'script -l python -- import sys; sys.path += [ "' ..
+                        vim.fn.expand("$HOME") .. '/Library/Python/3.9/lib/python/site-packages" ]')
                   end
                   table.insert(commands, "settings set target.load-script-from-symbol-file true")
                   return commands
