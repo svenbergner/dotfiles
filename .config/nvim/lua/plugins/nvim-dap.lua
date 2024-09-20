a8340ec1663456a1f36478ade0b1fad326c6d020
Sven Bergner <sven.bergner@gmail.com>
2024-09-20 17:16:51 +0200

    Display var name and content inside of angle brackets

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -222,7 +222,7 @@ return {
          highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
          highlight_new_as_changed = true,    -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
          show_stop_reason = true,            -- show stop reason when stopped for exceptions
-         commented = true,                   -- prefix virtual text with comment string
+         commented = false,                   -- prefix virtual text with comment string
          only_first_definition = false,      -- only show virtual text at first definition (if there are multiple)
          all_references = true,              -- show virtual text on all all references of the variable (not only definitions)
          clear_on_continue = false,          -- clear virtual text on "continue" (might cause flickering when stepping)
@@ -234,7 +234,7 @@ return {
          --- @param options nvim_dap_virtual_text_options Current options for nvim-dap-virtual-text
          --- @return string|nil A text how the virtual text should be displayed or nil, if this variable shouldn't be displayed
          display_callback = function(variable, buf, stackframe, node, options)
-            return variable.name .. ' = ' .. variable.value:gsub("%s+", " ")
+            return '<' .. variable.name .. ' = ' .. variable.value:gsub("%s+", " ") .. '>'
          end,
          virt_text_win_col = 120,
          virt_text_pos = "inline",
