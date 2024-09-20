201866ba4a8237def179641a966b8376a5186a87
Sven Bergner <sven.bergner@gmail.com>
2024-09-20 15:00:10 +0200

    nvim-rest plugin added

---
+++ .config/nvim/lua/plugins/nvim-rest.lua
@@ -0,0 +1,13 @@
+-- A very fast, powerful, extensible and asynchronous Neovim HTTP client written in Lua.
+-- https://github.com/rest-nvim/rest.nvim
+--
+-- rest.nvim by default makes use of its own curl wrapper made in pure Lua 
+-- and a tree-sitter parser to parse http files. So what you can run get exact 
+-- and detailed result what you see from your editor!
+--
+-- In addition to this, you can also write integrations with external 
+-- HTTP clients, such as the postman CLI.
+
+return {
+   "rest-nvim/rest.nvim",
+}
