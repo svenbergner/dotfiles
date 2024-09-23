70e333852f073d7a1a7edd41b3179256c60bfeeb
Sven Bergner <sven.bergner@gmail.com>
2024-09-23 11:37:03 +0200

    Added keymaps for logpoints and hover information

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -192,6 +192,12 @@ return {
          vim.keymap.set("n", "<leader>B", function()
             dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
          end, { desc = 'Set conditional breakpoint' })
+         vim.keymap.set('n', '<Leader>lp',
+            function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end,
+            { desc = 'Set [l]og [p]oint' })
+         vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
+            require('dap.ui.widgets').hover()
+         end, { desc = 'Show hover information' })
 
          dapui.setup({
             icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
@@ -222,7 +228,7 @@ return {
          highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
          highlight_new_as_changed = true,    -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
          show_stop_reason = true,            -- show stop reason when stopped for exceptions
-         commented = false,                   -- prefix virtual text with comment string
+         commented = false,                  -- prefix virtual text with comment string
          only_first_definition = false,      -- only show virtual text at first definition (if there are multiple)
          all_references = true,              -- show virtual text on all all references of the variable (not only definitions)
          clear_on_continue = false,          -- clear virtual text on "continue" (might cause flickering when stepping)
