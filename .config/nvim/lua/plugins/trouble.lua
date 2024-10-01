11a9392145b020fb11d0d60a02fc24c3d791b925
Sven Bergner <sven.bergner@gmail.com>
2024-10-01 11:07:47 +0200

    Githup link added

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,5 +1,7 @@
 -- A pretty list for showing diagnostics, references, telescope results,
 -- quickfix and location lists to help you solve all the trouble your code is causing.
+-- https://github.com/folke/trouble.nvim
+
 return {
    "folke/trouble.nvim",
    dependencies = { "nvim-tree/nvim-web-devicons" },
