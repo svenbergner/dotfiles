7d802e10ceeda932c55701f7fa27aaa79df841c9
Sven Bergner <sven.bergner@wolterskluwer.com>
2024-10-01 11:07:09 +0200

    Removed unused parameters

--- .config/wezterm/layouts/development.lua
+++ .config/wezterm/layouts/development.lua
@@ -9,31 +9,27 @@ M.setup_layout = function(wezterm)
       home_dir = '/C'
     end
 
-    local first_tab, first_pane, window = mux.spawn_window {
+    local first_tab, _, window = mux.spawn_window {
       workspace = 'Development',
       cwd = home_dir .. '/Repos/simply_tax_app',
     }
     first_tab:set_title('SimplyTaxApp')
-    first_pane:send_text('nvim .\n')
 
-    local second_tab, second_pane, _ = window:spawn_tab {
+    local second_tab, _, _ = window:spawn_tab {
       cwd = home_dir .. '/Repos/SSE/Dev',
     }
     second_tab:set_title('SSE/TaxCoreApi')
-    second_pane:send_text('nvim .\n')
 
-    local third_tab, third_pane, _ = window:spawn_tab {
+    local third_tab, _, _ = window:spawn_tab {
       cwd = home_dir .. '/Repos/webhook-builds',
     }
     third_tab:set_title('Build-Pipelines')
-    third_pane:send_text('nvim .\n')
 
 
-    local fourth_tab, fourth_pane, _ = window:spawn_tab {
+    local fourth_tab, _, _ = window:spawn_tab {
       cwd = home_dir .. '/Repos/Content/StP/30/DMSource',
     }
     fourth_tab:set_title('Content StP')
-    fourth_pane:send_text('nvim .\n')
 
     first_tab:activate()
     window:gui_window():maximize()
