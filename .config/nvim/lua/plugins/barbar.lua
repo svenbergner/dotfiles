c44aed4fd0314811ece00cdfa441cdce07cfb168
Sven Bergner <sven.bergner@gmail.com>
2024-10-30 16:22:06 +0100

    Renamed local var to be clear

--- .config/nvim/lua/plugins/barbar.lua
+++ .config/nvim/lua/plugins/barbar.lua
@@ -19,30 +19,30 @@ return {
       init = function()
          vim.g.barbar_auto_setup = false
          local map = vim.api.nvim_set_keymap
-         local opts = { noremap = true, silent = true }
+         local keymap_options = { noremap = true, silent = true }
 
          -- Move to previous/next
-         map('n', '<M-,>', '<Cmd>BufferPrevious<CR>', opts)
-         map('n', '<M-.>', '<Cmd>BufferNext<CR>', opts)
+         map('n', '<M-,>', '<Cmd>BufferPrevious<CR>', keymap_options)
+         map('n', '<M-.>', '<Cmd>BufferNext<CR>', keymap_options)
          -- Re-order to previous/next
-         map('n', '<M-<>', '<Cmd>BufferMovePrevious<CR>', opts)
-         map('n', '<M->>', '<Cmd>BufferMoveNext<CR>', opts)
+         map('n', '<M-<>', '<Cmd>BufferMovePrevious<CR>', keymap_options)
+         map('n', '<M->>', '<Cmd>BufferMoveNext<CR>', keymap_options)
          -- Goto buffer in position...
-         map('n', '<M-1>', '<Cmd>BufferGoto 1<CR>', opts)
-         map('n', '<M-2>', '<Cmd>BufferGoto 2<CR>', opts)
-         map('n', '<M-3>', '<Cmd>BufferGoto 3<CR>', opts)
-         map('n', '<M-4>', '<Cmd>BufferGoto 4<CR>', opts)
-         map('n', '<M-5>', '<Cmd>BufferGoto 5<CR>', opts)
-         map('n', '<M-6>', '<Cmd>BufferGoto 6<CR>', opts)
-         map('n', '<M-7>', '<Cmd>BufferGoto 7<CR>', opts)
-         map('n', '<M-8>', '<Cmd>BufferGoto 8<CR>', opts)
-         map('n', '<M-9>', '<Cmd>BufferGoto 9<CR>', opts)
-         map('n', '<M-0>', '<Cmd>BufferLast<CR>', opts)
+         map('n', '<M-1>', '<Cmd>BufferGoto 1<CR>', keymap_options)
+         map('n', '<M-2>', '<Cmd>BufferGoto 2<CR>', keymap_options)
+         map('n', '<M-3>', '<Cmd>BufferGoto 3<CR>', keymap_options)
+         map('n', '<M-4>', '<Cmd>BufferGoto 4<CR>', keymap_options)
+         map('n', '<M-5>', '<Cmd>BufferGoto 5<CR>', keymap_options)
+         map('n', '<M-6>', '<Cmd>BufferGoto 6<CR>', keymap_options)
+         map('n', '<M-7>', '<Cmd>BufferGoto 7<CR>', keymap_options)
+         map('n', '<M-8>', '<Cmd>BufferGoto 8<CR>', keymap_options)
+         map('n', '<M-9>', '<Cmd>BufferGoto 9<CR>', keymap_options)
+         map('n', '<M-0>', '<Cmd>BufferLast<CR>', keymap_options)
          -- Pin/unpin buffer
-         map('n', '<M-p>', '<Cmd>BufferPin<CR>', opts)
+         map('n', '<M-p>', '<Cmd>BufferPin<CR>', keymap_options)
          -- Close buffer
-         map('n', '<M-c>', '<Cmd>BufferClose<CR>', opts)
-         map('n', '<M-x>', '<Cmd>BufferClose<CR>', opts)
+         map('n', '<M-c>', '<Cmd>BufferClose<CR>', keymap_options)
+         map('n', '<M-x>', '<Cmd>BufferClose<CR>', keymap_options)
          -- Wipeout buffer
          --                 :BufferWipeout
          -- Close commands
@@ -52,13 +52,13 @@ return {
          --                 :BufferCloseBuffersLeft
          --                 :BufferCloseBuffersRight
          -- Magic buffer-picking mode
-         -- map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+         -- map('n', '<C-p>', '<Cmd>BufferPick<CR>', keymap_options)
          -- -- Sort automatically by...
-         -- map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
-         -- map('n', '<Space>bn', '<Cmd>BufferOrderByName<CR>', opts)
-         -- map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
-         -- map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
-         -- map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+         -- map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', keymap_options)
+         -- map('n', '<Space>bn', '<Cmd>BufferOrderByName<CR>', keymap_options)
+         -- map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', keymap_options)
+         -- map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', keymap_options)
+         -- map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', keymap_options)
       end,
       opts = {
          animation = true,
