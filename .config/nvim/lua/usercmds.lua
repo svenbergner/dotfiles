75cec43b69ac6b1de745f9ef2e919f54657538b9
Sven Bergner <sven.bergner@gmail.com>
2024-11-07 15:02:01 +0100

    Write file and quit highlight search

--- .config/nvim/lua/usercmds.lua
+++ .config/nvim/lua/usercmds.lua
@@ -1,4 +1,6 @@
 -- Remove divider comments
 vim.api.nvim_create_user_command('RemoveDividerComments', function ()
    vim.api.nvim_command('silent! %s/^\\/\\/\\**\\n//')
+   vim.api.nvim_command('write')
+   vim.api.nvim_command('nohlsearch')
 end, {nargs = 0})
