b19db2a342bd9cf4def97349504f69fa4be24534
Sven Bergner <sven.bergner@gmail.com>
2024-11-08 17:56:37 +0100

    Fixed noice settings

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,4 +1,4 @@
--- Highly experimental plugin that completely replaces the UI 
+-- Highly experimental plugin that completely replaces the UI
 -- for messages, cmdline and the popupmenu.
 return {
    "folke/noice.nvim",
@@ -8,10 +8,10 @@ return {
       { "<leader>nsh", function() require("noice").cmd("history") end,                                desc = "[N]oice [S]how [H]istory" },
       { "<leader>nsa", function() require("noice").cmd("all") end,                                    desc = "[N]oice [S]how [A]ll" },
       { "<leader>nda", function() require("noice").cmd("dismiss") end,                                desc = "[N]oice [D]ismiss [A]ll" },
-      { '<leader>sn',  function() require("telescope").extensions.notify.notify() end,                desc = '[S]earch [N]otificanions'  },
-      { "<S-Enter>",   function() require("noice").redirect(vim.fn.getcmdline()) end,                 desc = "Redirect Cmdline", mode = "c" },
-      { "<c-f>",       function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end,  desc = "Scroll forward",   mode = { "i", "n", "s" },  silent = true, expr = true, },
-      { "<c-b>",       function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, desc = "Scroll backward",  mode = { "i", "n", "s" },  silent = true, expr = true, },
+      { '<leader>sn',  function() require("telescope").extensions.notify.notify() end,                desc = '[S]earch [N]otificanions' },
+      { "<S-Enter>",   function() require("noice").redirect(vim.fn.getcmdline()) end,                 desc = "Redirect Cmdline",             mode = "c" },
+      { "<c-f>",       function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end,  desc = "Scroll forward",               mode = { "i", "n", "s" }, silent = true, expr = true, },
+      { "<c-b>",       function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, desc = "Scroll backward",              mode = { "i", "n", "s" }, silent = true, expr = true, },
    },
    config = function()
       require('noice').setup({
@@ -21,6 +21,20 @@ return {
                ["vim.lsp.util.stylize_markdown"] = true,
                ["cmp.entry.get_documentation"] = true,
             },
+         },
+         routes = {
+            {
+               view = "notify",
+               filter = {
+                  event = "msg_showmode"
+               },
+            },
+         },
+         presets = {
+            bottom_search = true,
+            command_palette = true,
+            long_message_to_split = true,
+            inc_rename = true,
          },
          views = {
             cmdline_popup = {
@@ -40,20 +54,6 @@ return {
                win_options = {
                   winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
                },
-            },
-            routes = {
-               {
-                  view = "notify",
-                  filter = {
-                     event = "msg_showmode"
-                  },
-               },
-            },
-            presets = {
-               bottom_search = true,
-               command_palette = true,
-               long_message_to_split = true,
-               inc_rename = true,
             },
          },
       })
