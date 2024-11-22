39bb57aa43e151add7fb687b9f81f9915f18a40a
Sven Bergner <sven.bergner@gmail.com>
2024-11-22 12:17:39 +0100

    Added mini.operators plugin

---
+++ .config/nvim/lua/plugins/mini-operators.lua
@@ -0,0 +1,27 @@
+--[=====[
+Text edit operators
+https://github.com/echasnovski/mini.operators
+
+Features:
+ - Operators:
+   - Evaluate text and replace with output.
+   - Exchange text regions.
+   - Multiply (duplicate) text.
+   - Replace text with register.
+   - Sort text.
+
+- Automated configurable mappings to operate on textobject, line, selection. 
+  Can be disabled in favor of more control with MiniOperators.make_mappings().
+
+- All operators support [count] and dot-repeat.
+
+See *MiniOperators-overview* and *MiniOperators.config* tags in help for more details.
+--]=====]
+
+return {
+   'echasnovski/mini.operators',
+   version = '*',
+   config = function()
+      require('mini.operators').setup()
+   end,
+}
