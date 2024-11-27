63d50cb60a992e27e02b042e078781aad0cecb46
Sven Bergner <sven.bergner@wolterskluwer.com>
2024-11-27 10:03:12 +0100

    Added mini.hipattern instead of nvim-colorizer

---
+++ .config/nvim/lua/plugins/mini-hipatterns.lua
@@ -0,0 +1,30 @@
+-- https://github.com/echasnovski/mini.hipatterns
+--
+return {
+   'echasnovski/mini.hipatterns',
+   version = '*',
+   config = function()
+      local hipatterns = require('mini.hipatterns')
+      local words = { ['enabled = true'] = '#B8BB26', ['enabled = false'] = '#CC241D' }
+
+      local word_color_group = function(_, match)
+         local hex = words[match]
+         if hex == nil then return nil end
+         return hipatterns.compute_hex_color_group(hex, 'bg')
+      end
+
+      require('mini.hipatterns').setup(
+         {
+            highlighters = {
+               word_color = {
+                  pattern = {
+                     'enabled = true',
+                     'enabled = false'
+                  },
+                  group = word_color_group
+               },
+            }
+         }
+      )
+   end,
+}
