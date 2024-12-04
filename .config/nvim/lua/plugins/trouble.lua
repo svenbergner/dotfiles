555835c0dbc398e340793f7f1c9a5400028f7337
Sven Bergner <sven.bergner@gmail.com>
2024-12-04 15:20:12 +0100

    Changed keymappings for trouble

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -10,12 +10,12 @@ return {
    dependencies = { "nvim-tree/nvim-web-devicons" },
    cmd = "Trouble",
    keys = {
-      { "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>", desc = "Diagnostics (Trouble)" },
-      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Buffer Diagnostics (Trouble)" },
-      { "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Location List (Trouble)" },
-      { "<leader>xq", "<cmd>Trouble qflist toggle<CR>", desc = "Quickfix List (Trouble)" },
-      { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<CR>", desc = "Symbols (Trouble)" },
-      { "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<CR>", desc = "Symbols (Trouble)" },
+      { "<leader><leader>td", "<cmd>Trouble diagnostics toggle<CR>", desc = "Diagnostics (Trouble)" },
+      { "<leader><leader>tD", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Buffer Diagnostics (Trouble)" },
+      { "<leader><leader>tl", "<cmd>Trouble loclist toggle<CR>", desc = "Location List (Trouble)" },
+      { "<leader><leader>tq", "<cmd>Trouble qflist toggle<CR>", desc = "Quickfix List (Trouble)" },
+      { "<leader><leader>ts", "<cmd>Trouble symbols toggle focus=false<CR>", desc = "Symbols (Trouble)" },
+      { "<leader><leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<CR>", desc = "Symbols (Trouble)" },
    },
    opts = {
       use_diagnostic_signs = true,
