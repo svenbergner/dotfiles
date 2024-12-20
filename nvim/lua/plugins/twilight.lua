9a53690e5a4b59835a2b9be2b073e9a6bdfac963
Sven Bergner <sven.bergner@wolterskluwer.com>
2024-12-20 13:53:23 +0100

    Removed because it's a part of snacks

--- nvim/lua/plugins/twilight.lua
+++
@@ -1,9 +0,0 @@
--- Twilight dims inactive portions of the code you're editing.
-
-return {
-   "folke/twilight.nvim",
-   opts = {},
-   config = function()
-      require('twilight').setup()
-   end
-}
