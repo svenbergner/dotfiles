e91c3be7b5b5673e695a3ca7b06d6e4b1971e45c
Sven Bergner <sven.bergner@gmail.com>
2024-12-27 20:56:51 +0100

    Disabled presenting in favor for my own presenter plugin

---
+++ nvim/lua/plugins/presenter.lua
@@ -0,0 +1,11 @@
+--[=====[
+Show markdown file as a slide presentation.
+It's based on the advent of neovim 2024 youtube-series by TJ DeVries.
+URL: https://github.com/svenbergner/presenter.nvim
+--]=====]
+
+return {
+   "svenbergner/presenter.nvim",
+   enabled = true,
+   dev = true,
+}
--- nvim/lua/plugins/presenting.lua
+++ nvim/lua/plugins/presenting.lua
@@ -5,6 +5,7 @@ URL: https://github.com/sotte/presenting.nvim
 
 return {
    'sotte/presenting.nvim',
+   enabled = false,
    config = function()
       require('presenting').setup()
    end,
