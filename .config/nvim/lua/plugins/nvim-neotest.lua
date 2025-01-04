7e37890f274788de498968e0ff22597cb1f393d7
Sven Bergner <sven.bergner@gmail.com>
2025-01-04 12:38:50 +0100

    Streamlined testing shortcuts

--- .config/nvim/lua/plugins/nvim-neotest.lua
+++ .config/nvim/lua/plugins/nvim-neotest.lua
@@ -31,33 +31,30 @@ return {
          }
       })
 
-      vim.keymap.set("n", "<leader>t", function()
+      vim.keymap.set("n", "<leader>tn", function()
          vim.cmd('wa')
          neotest.run.run()
-      end, { desc = 'Run nearest [t]est' });
-      -- Attach to the nearest test, see :h neotest.run.attach()
+      end, { desc = '[t]est: run [n]earest' });
+
+      vim.keymap.set("n", "<leader>ts", function()
+         neotest.run.stop()
+      end, { desc = '[t]est: [s]top running test' });
+
       vim.keymap.set("n", "<leader>ta", function()
          vim.cmd('wa')
-         neotest.run.run()
-      end, { desc = 'Nearest [t]est [a]ttach' });
-      -- Stop the nearest test, see :h neotest.run.stop()
-      vim.keymap.set("n", "<leader>ts", function() neotest.run.stop() end, { desc = 'Nearest [t]est [s]top' });
-      vim.keymap.set("n", "<leader>T", function()
-            vim.cmd('wa')
-            neotest.run.run(vim.fn.expand("%"))
-         end, { desc = 'Run all [T]ests in file' });
-      -- Debug the nearest test (requires nvim-dap and adapter support)
-      vim.keymap.set("n", "<leader>dt", function() neotest.run.run({ strategy = "dap" }) end,
-         { desc = '[d]ebug nearest [t]est' });
-      vim.keymap.set("n", "<leader>t", function() neotest.run.run() end, { desc = 'Run nearest [t]est' });
-      vim.keymap.set("n", "<leader>tr",
-         function() neotest.output.open({ enter = true, quiet = true, auto_close = true, last_run = true }) end,
-         { desc = 'Show [t]est [r]esults' });
-      vim.keymap.set("n", "<leader>tst",
-         function() neotest.summary.toggle() end,
-         { desc = '[t]est [s]ummary [t]oggle' });
-      -- vim.keymap.set("n", "<leader>a", function()  end, { desc = 'Run [a]ll [t]ests in suite' });
-      -- vim.keymap.set("n", "<leader>l", function()  end, { desc = 'Run [l]ast test' });
-      -- vim.keymap.set("n", "<leader>g", function()  end, { desc = '[g]oto last test' });
+         neotest.run.run(vim.fn.expand("%"))
+      end, { desc = '[T]ests run [a]ll in file' });
+
+      vim.keymap.set("n", "<leader>td", function()
+         neotest.run.run({ strategy = "dap" })
+      end, { desc = '[d]ebug nearest [t]est' });
+
+      vim.keymap.set("n", "<leader>tr", function()
+         neotest.output.open({ enter = true, quiet = true, auto_close = true, last_run = true })
+      end, { desc = 'Show [t]est [r]esults' });
+
+      vim.keymap.set("n", "<leader>tst", function()
+         neotest.summary.toggle()
+      end, { desc = '[t]est [s]ummary [t]oggle' });
    end,
 }
