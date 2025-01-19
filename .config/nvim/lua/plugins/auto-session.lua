55c5745965d7d214a3cf85d5d60a7388e7fa6ee8
Sven Bergner <sven.bergner@gmail.com>
2025-01-19 13:27:13 +0100

    Header added

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -1,5 +1,7 @@
--- Automatically manage vim sessions
--- URL: https://www.github.com/rmagatti/auto-session
+--[=====[
+Automatically manage vim sessions
+URL: https://www.github.com/rmagatti/auto-session
+--]=====]
 
 return {
    'rmagatti/auto-session',
--- .config/nvim/lua/plugins/helpview.lua
+++ .config/nvim/lua/plugins/helpview.lua
@@ -1,14 +1,17 @@
--- Decorations for vimdoc/help files in Neovim.
--- URL: https://github.com/OXY2DEV/helpview.nvim
+--[=====[
+Decorations for vimdoc/help files in Neovim.
+URL: https://github.com/OXY2DEV/helpview.nvim
+--]=====]
+
 return {
-    "OXY2DEV/helpview.nvim",
-    lazy = false, -- Recommended
+   "OXY2DEV/helpview.nvim",
+   lazy = false,  -- Recommended
+   enabled = true, -- Recommended
 
-    -- In case you still want to lazy load
-    -- ft = "help",
+   -- In case you still want to lazy load
+   -- ft = "help",
 
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter"
-    }
+   dependencies = {
+      "nvim-treesitter/nvim-treesitter"
+   }
 }
-
