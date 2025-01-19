e80aa4917238cb0fb8c351cb7512bdd8c012e74a
Sven Bergner <sven.bergner@wolterskluwer.com>
2025-01-19 13:27:13 +0100

    Header added

--- .config/nvim/lua/plugins/autoclose.lua
+++ .config/nvim/lua/plugins/autoclose.lua
@@ -1,6 +1,11 @@
--- Automatically add the closing element
+--[=====[
+Automatically add the closing element
+URL: https://www.github.com/m4xshen/autoclose.nvim
+--]=====]
+
 return {
    "m4xshen/autoclose.nvim",
    event = 'VeryLazy',
+   enabled = true,
    opts = {},
 }
