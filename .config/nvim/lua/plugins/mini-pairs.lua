f2f23ae261045c6ccf35ca778a8c67226b903c67
Sven Bergner <sven.bergner@gmail.com>
2025-01-25 22:19:36 +0100

    Removed autoclose for mini.pairs

---
+++ .config/nvim/lua/plugins/mini-pairs.lua
@@ -0,0 +1,10 @@
+--[===[ 
+Minimal and fast autopairs
+URL: https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-pairs.md 
+--]===]
+
+return {
+   'echasnovski/mini.pairs',
+   version = false,
+   enabled = true,
+}
