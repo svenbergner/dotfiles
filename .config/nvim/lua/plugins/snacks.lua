ff6b30fb1a15b046d43bbf74ffe6632857082b73
Sven Bergner <sven.bergner@gmail.com>
2025-02-03 08:53:23 +0100

    Feature toggle keymaps added

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -150,4 +150,34 @@ return {
       { "<leader>Ws", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
       { "<leader>gs", function() Snacks.picker.git_status() end,            desc = "[g]it [s]tatus" },
    },
+   init = function()
+      vim.api.nvim_create_autocmd("User", {
+         pattern = "VeryLazy",
+         callback = function()
+            -- Setup some globals for debugging (lazy-loaded)
+            _G.dd = function(...)
+               Snacks.debug.inspect(...)
+            end
+            _G.bt = function()
+               Snacks.debug.backtrace()
+            end
+            vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+            -- Create some toggle mappings
+            Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+            Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+            Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>ul")
+            Snacks.toggle.diagnostics():map("<leader>ud")
+            Snacks.toggle.line_number():map("<leader>uL")
+            Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+                :map("<leader>uc")
+            Snacks.toggle.treesitter():map("<leader>uT")
+            Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map(
+            "<leader>ub")
+            Snacks.toggle.inlay_hints():map("<leader>uh")
+            Snacks.toggle.indent():map("<leader>ug")
+            Snacks.toggle.dim():map("<leader>uD")
+         end,
+      })
+   end,
 }
