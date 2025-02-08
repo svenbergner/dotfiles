3b4f237e0b061aebb9ee5489f3c6101c3ae3b834
Sven Bergner <sven.bergner@gmail.com>
2025-02-08 17:02:18 +0100

    Some options added for nicer completion

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -87,9 +87,22 @@ return {
       local servers = {
          bashls = {},
          clangd = {
+            capabilities = {
+               offsetEncoding = { "utf-16" },
+            },
             cmd = {
                "clangd",
-               "--offset-encoding=utf-16",
+               "--background-index",
+               "--clang-tidy",
+               "--header-insertion=iwyu",
+               "--completion-style=detailed",
+               "--function-arg-placeholders",
+               "--fallback-style=llvm",
+            },
+            init_options = {
+               usePlaceholders = true,
+               completeUnimported = true,
+               clangdFileStatus = true,
             },
             inlay_hints = {
                includeInlayEnumMemberValueHints = true,
