c54027a1899699c53630960ea5432f8c7c6df4a9
Sven Bergner <sven.bergner@gmail.com>
2025-02-09 22:20:35 +0100

    Added missing url and cleanups

--- .config/nvim/lua/plugins/funwithvim.lua
+++ .config/nvim/lua/plugins/funwithvim.lua
@@ -6,40 +6,26 @@ Collection of plugins that are fun to use
 4. URL: https://github.com/rhysd/vim-syntax-christmas-tree
 5. URL: https://github.com/AndrewRadev/dealwithit.vim
 6. URL: https://github.com/folke/drop.nvim 
+7. URL: https://github.com/ThePrimeagen/vim-be-good
 --]===]
 
 return {
-   -- {
-   --    'eandrju/cellular-automaton.nvim'
-   -- },
-   -- {
-   --    'tamton-aquib/duck.nvim',
+   -- { 'eandrju/cellular-automaton.nvim' },
+   -- { 'tamton-aquib/duck.nvim',
    --    config = function()
    --       vim.keymap.set('n', '<leader>dd', function() require("duck").hatch() end, {})
    --       vim.keymap.set('n', '<leader>dk', function() require("duck").cook() end, {})
    --       vim.keymap.set('n', '<leader>da', function() require("duck").cook_all() end, {})
    --    end
    -- },
-   -- {
-   --    'AndrewRadev/typewriter.vim'
-   -- },
-   {
-      'rhysd/vim-syntax-christmas-tree',
-      enabled = false,
-      -- Shows a christmas tree in a split with :MerryChristmas
-   },
-   -- {
-   --    'AndrewRadev/dealwithit.vim'
-   -- },
-   -- {
-   --    "folke/drop.nvim",
+   -- { 'AndrewRadev/typewriter.vim' },
+   -- { 'rhysd/vim-syntax-christmas-tree', -- Shows a christmas tree in a split with :MerryChristmas },
+   -- { 'AndrewRadev/dealwithit.vim' },
+   -- { "folke/drop.nvim",
    --    opt = {
    --       screensaver = 1000 * 60 * 5, -- show after 5 minutes. Set to false, to disable
    --       filetype = { "markdown", "vimwiki", "text", "lua" },
    --    },
    -- },
-   -- {
-   --    "ThePrimeagen/vim-be-good",
-   --    event = "VeryLazy",
-   -- }
+   -- { "ThePrimeagen/vim-be-good", }
 }
