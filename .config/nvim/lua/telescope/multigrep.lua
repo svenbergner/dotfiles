743f72de8c4f4aad403c0201a08ab86c211a2e4d
Sven Bergner <sven.bergner@gmail.com>
2025-02-11 13:54:38 +0100

    Added description

--- .config/nvim/lua/telescope/multigrep.lua
+++ .config/nvim/lua/telescope/multigrep.lua
@@ -8,50 +8,50 @@ local previewers = require('telescope.previewers')
 local sorters = require('telescope.sorters')
 
 local live_multigrep = function(opts)
-  opts = opts or {}
-  opts.cwd = opts.cwd or vim.uv.cwd()
+   opts = opts or {}
+   opts.cwd = opts.cwd or vim.uv.cwd()
 
-  local finder = finders.new_async_job {
-    command_generator = function(prompt)
-      if not prompt or prompt == '' then
-        return nil
-      end
+   local finder = finders.new_async_job {
+      command_generator = function(prompt)
+         if not prompt or prompt == '' then
+            return nil
+         end
 
-      local pieces = vim.split(prompt, '  ')
-      local args = { 'rg' }
+         local pieces = vim.split(prompt, '  ')
+         local args = { 'rg' }
 
-      if pieces[1] then
-        table.insert(args, '-e')
-        table.insert(args, pieces[1])
-      end
+         if pieces[1] then
+            table.insert(args, '-e')
+            table.insert(args, pieces[1])
+         end
 
-      if pieces[2] then
-        table.insert(args, '-g')
-        table.insert(args, pieces[2])
-      end
+         if pieces[2] then
+            table.insert(args, '-g')
+            table.insert(args, pieces[2])
+         end
 
-      ---@diagnostic disable-next-line: deprecated
-      return vim.tbl_flatten { args,
-        { '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case' },
-      }
-    end,
-    entry_maker = make_entry.gen_from_vimgrep(opts),
-    cwd = opts.cwd,
-  }
+         ---@diagnostic disable-next-line: deprecated
+         return vim.tbl_flatten { args,
+            { '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case' },
+         }
+      end,
+      entry_maker = make_entry.gen_from_vimgrep(opts),
+      cwd = opts.cwd,
+   }
 
-  pickers.new(opts, {
-    debounce = 100,
-    prompt_title = 'Live Multigrep',
-    finder = finder,
-    -- previewer = conf.grep_previewer(opts),
-    previewer = previewers.vimgrep.new(opts),
-    sorter = sorters.empty(),
-  }):find()
+   pickers.new(opts, {
+      debounce = 100,
+      prompt_title = 'Live Multigrep',
+      finder = finder,
+      -- previewer = conf.grep_previewer(opts),
+      previewer = previewers.vimgrep.new(opts),
+      sorter = sorters.empty(),
+   }):find()
 end
 
 M.setup = function()
-  print('Setting up live multigrep')
-  vim.keymap.set('n', '<leader>fm', live_multigrep)
+   print('Setting up live multigrep')
+   vim.keymap.set('n', '<leader>fm', live_multigrep, { noremap = true, desc = '[f]ind [m]ultigrep' })
 end
 
 return M
