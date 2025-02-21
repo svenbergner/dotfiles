01e84996a099fb5963f7bec43fb815de26cb603e
Sven Bergner <sven.bergner@wolterskluwer.com>
2025-02-21 17:43:18 +0100

    Added keymaps for adding list items

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -6,6 +6,10 @@ vim.api.nvim_create_autocmd("BufEnter", {
    callback = function()
       vim.keymap.set('n', '<leader><F5>', '<cmd>PresenterStart<CR>',
          { silent = true, desc = "Starts a presentation of the current markdownfile" })
+      vim.keymap.set('n', '<leader>mc', '^I-<Space>[<Space>]<Space><Esc>^j',
+         { remap = true, silent = false, desc = "[m]arkdown [c]hackable list item" })
+      vim.keymap.set('n', '<leader>ml', '^I-<Space><Esc>^j',
+         { remap = true, silent = false, desc = "[m]arkdown [l]ist item" })
    end,
    desc = "Starts a presentation of the current markdownfile",
 })
