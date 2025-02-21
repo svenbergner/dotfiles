79ccd62c3cc5d6d099d5cc9073951ef678d8331a
Sven Bergner <sven.bergner@gmail.com>
2025-02-21 17:23:52 +0100

    Coding style

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,11 +1,11 @@
 local group_id = vim.api.nvim_create_augroup("StartMarkdownPresentation", { clear = true })
 
 vim.api.nvim_create_autocmd("BufEnter", {
-    group = group_id,
-    pattern = "*.md",
-    callback = function()
-        vim.keymap.set('n', '<leader><F5>', '<cmd>PresenterStart<CR>', { silent = true, desc = "Starts a presentation of the current markdownfile" })
-    end,
-    desc = "Starts a presentation of the current markdownfile",
+   group = group_id,
+   pattern = "*.md",
+   callback = function()
+      vim.keymap.set('n', '<leader><F5>', '<cmd>PresenterStart<CR>',
+         { silent = true, desc = "Starts a presentation of the current markdownfile" })
+   end,
+   desc = "Starts a presentation of the current markdownfile",
 })
-
