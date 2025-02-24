70f474d6db2101591f0eacc343ae08978ff073cc
Sven Bergner <sven.bergner@gmail.com>
2025-02-24 09:54:47 +0100

    Added tab and s-tab keymaps for more conveniance

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -47,7 +47,11 @@ return {
       -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
       -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
       -- See the full "keymap" documentation for information on defining your own keymap.
-      keymap = { preset = 'enter' },
+      keymap = {
+         preset = 'enter',
+         ['<Tab>'] = { 'select_next', 'fallback' },
+         ['<S-Tab>'] = { 'select_prev', 'fallback' },
+      },
 
       appearance = {
          -- Sets the fallback highlight groups to nvim-cmp's highlight groups
