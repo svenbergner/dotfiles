8e00114bc6f17a8053b430a88926035a9c74fb03
Sven Bergner <sven.bergner@gmail.com>
2025-03-06 09:37:16 +0100

    Plugin description added

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,8 +1,31 @@
 --[===[
-install without yarn or npm
+Markdown Preview for (Neo)vim
+Preview Markdown in your modern browser with synchronised scrolling and flexible configuration.
+
+Main features:
+
+- Cross platform (MacOS/Linux/Windows)
+- Synchronised scrolling
+- Fast asynchronous updates
+- KaTeX for typesetting of math
+- PlantUML
+- Mermaid
+- Chart.js
+- js-sequence-diagrams
+- Flowchart
+- dot
+- Table of contents
+- Emojis
+- Task lists
+- Local images
+- Flexible configuration
+
+NOTE: the plugin mathjax-support-for-mkdp is not needed for typesetting math.
+
 URL: https://github.com/iamcco/markdown-preview.nvim 
 --]===]
 
+-- install without yarn or npm
 return {
   "iamcco/markdown-preview.nvim",
   enabled = true,
@@ -12,7 +35,7 @@ return {
   build = function() vim.fn["mkdp#util#install"]() end,
 }
 
--- -- install with yarn or npm
+-- install with yarn or npm
 -- {
 --   "iamcco/markdown-preview.nvim",
 --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
