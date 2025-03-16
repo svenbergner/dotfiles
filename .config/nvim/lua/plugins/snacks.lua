4a4ed630230bd03f184cf7cd13195fd78baf9a27
Sven Bergner <sven.bergner@gmail.com>
2025-03-16 11:18:16 +0100

    Fixed spellings

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -60,17 +60,17 @@ return {
       scroll = { enabled = true, },
       statuscolumn = {
          enabled = true,
-         left = { "mark", "sign" }, -- priority of signs on the left (high to low)
-         right = { "fold", "git" }, -- priority of signs on the right (high to low)
+         left = { "mark", "sign" }, -- Priority of signs on the left (high to low)
+         right = { "fold", "git" }, -- Priority of signs on the right (high to low)
          folds = {
             open = true,            -- show open fold icons
-            git_hl = true,          -- use Git Signs hl for fold icons
+            git_hl = true,          -- Use Git Signs hl for fold icons
          },
          git = {
             -- patterns to match Git signs
             patterns = { "GitSign", "MiniDiffSign" },
          },
-         refresh = 50, -- refresh at most every 50ms
+         refresh = 50, -- refresh at most every 50 milliseconds
       },
       terminal = { enabled = true, },
       toggle = { enabled = true, },
