49bd852eea5046077ff55997a258f13019fee9e3
Sven Bergner <sven.bergner@gmail.com>
2025-03-23 12:41:10 +0100

    Keymap for finding registers addded

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -114,6 +114,7 @@ return {
       { "<leader>/",  function() Snacks.picker.lines() end,              desc = "[/] fuzzy search in buffer" },
       { "<leader>fo", function() Snacks.picker.recent() end,             desc = "[f]ind [o]ld files" },
       { "<leader>fr", function() Snacks.picker.resume() end,             desc = "[f]ind [r]esume last search" },
+      { "<leader>fR", function() Snacks.picker.registers() end,          desc = "[f]ind [r]egisters" },
       { "<leader>ft", function() Snacks.picker.todo_comments() end,      desc = "[f]ind [t]odos" },
       { "<leader>fu", function() Snacks.picker.undo() end,               desc = "[f]ind [u]ndo history" },
       { "<leader>fz", function() Snacks.picker.zoxide() end,             desc = "[f]ind [z]oxide" },
