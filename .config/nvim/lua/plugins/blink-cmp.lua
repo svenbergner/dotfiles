080ac47fd4a33ad6ebbafa40b9f4ff145e0358ab
Sven Bergner <sven.bergner@gmail.com>
2025-03-30 12:24:44 +0200

    Removed to remove warning

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -189,7 +189,6 @@ return {
             env = {
                name = "Env",
                module = "blink-cmp-env",
-               --- @type blink-cmp-env.Options
                opts = {
                   -- item_kind = require("blink.cmp.types").CompletionItemKind.Variable,
                   show_braces = false,
