af05b2f3a9de5a9889e274d340e1d80802f6f078
Sven Bergner <sven.bergner@gmail.com>
2025-04-03 09:45:10 +0200

    Neovim 0.11 native completion support activated

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -239,13 +239,26 @@ return {
                capabilities = capabilities,
                settings = servers[server_name],
                handlers = {
-                  ["textDocument/hover"] = vim.lsp.buf.hover, { border = 'single' },
-                  ["textDocument/signatureHelp"] = vim.lsp.buf.signature_help, { border = 'single' }
+                  ["textDocument/hover"] = vim.lsp.buf.hover,
+                  { border = 'single' },
+                  ["textDocument/signatureHelp"] = vim.lsp.buf.signature_help,
+                  { border = 'single' },
                }
             })
          end,
       }
 
       vim.api.nvim_command('MasonToolsInstall')
+      vim.api.nvim_create_autocmd('LspAttach', {
+         callback = function(ev)
+            local client = vim.lsp.get_client_by_id(ev.data.client_id)
+            if client == nil then
+               return
+            end
+            if client:supports_method('textDocument/completion') then
+               vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+            end
+         end,
+      })
    end
 }
