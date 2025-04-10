4b37ea0149a375123f076609b321127e553957e9
Sven Bergner <sven.bergner@gmail.com>
2025-04-10 09:58:15 +0200

    Removed conflicting code

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -250,16 +250,5 @@ return {
       }
 
       vim.api.nvim_command('MasonToolsInstall')
-      vim.api.nvim_create_autocmd('LspAttach', {
-         callback = function(ev)
-            local client = vim.lsp.get_client_by_id(ev.data.client_id)
-            if client == nil then
-               return
-            end
-            if client:supports_method('textDocument/completion') then
-               vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-            end
-         end,
-      })
    end
 }
