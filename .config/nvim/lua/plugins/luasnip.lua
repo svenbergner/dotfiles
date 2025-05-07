984b2794e22487b0de18936ce0fa82c9049cc8bf
Sven Bergner <sven.bergner@gmail.com>
2025-05-07 12:15:54 +0200

    feat(nvim): Added personal snippets to blink-cmp/luasnip

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -2,158 +2,163 @@
 These are custom snippets created using LuaSnip
 
 I took the idea from this video:
-URL: https://youtu.be/FmHhonPjvvA?si=8NrcRWu4GGdmTzee 
+URL: https://youtu.be/FmHhonPjvvA?si=8NrcRWu4GGdmTzee
 --]===]
 
 return {
-  "L3MON4D3/LuaSnip",
-  enabled = true,
-  opts = function(_, opts)
-    local ls = require("luasnip")
+   "L3MON4D3/LuaSnip",
+   enabled = true,
+   opts = function(_, opts)
+      local ls = require("luasnip")
 
-    -- Preserve existing opts from LazyVim
-    opts = vim.tbl_deep_extend("force", opts, {
-      history = true,
-      delete_check_events = "TextChanged",
-    })
+      -- Load my personal snippets
+      local snippets_path = vim.fn.stdpath('config') .. "/snippets"
+      print("Loading snippets from: " .. snippets_path)
+      require("luasnip.loaders.from_vscode").lazy_load({ paths = { snippets_path } })
 
-    local s = ls.snippet
-    local t = ls.text_node
-    local i = ls.insert_node
-    local f = ls.function_node
-
-    local function clipboard()
-      return vim.fn.getreg("+")
-    end
-
-    -- #####################################################################
-    --                            Markdown
-    -- #####################################################################
-
-    -- Helper function to create code block snippets
-    local function create_code_block_snippet(lang)
-      return s(lang, {
-        t({ "```" .. lang, "" }),
-        i(1),
-        t({ "", "```" }),
+      -- Preserve existing opts from LazyVim
+      opts = vim.tbl_deep_extend("force", opts, {
+         history = true,
+         delete_check_events = "TextChanged",
       })
-    end
 
-    -- Define languages for code blocks
-    local languages = {
-      "txt",
-      "lua",
-      "sql",
-      "go",
-      "regex",
-      "bash",
-      "markdown",
-      "markdown_inline",
-      "yaml",
-      "json",
-      "jsonc",
-      "cpp",
-      "csv",
-      "java",
-      "javascript",
-      "python",
-      "dockerfile",
-      "html",
-      "css",
-      "templ",
-      "php",
-    }
+      local s = ls.snippet
+      local t = ls.text_node
+      local i = ls.insert_node
+      local f = ls.function_node
 
-    -- Generate snippets for all languages
-    local markdown_snippets = {}
+      local function clipboard()
+         return vim.fn.getreg("+")
+      end
 
-    for _, lang in ipairs(languages) do
-      table.insert(markdown_snippets, create_code_block_snippet(lang))
-    end
+      -- #####################################################################
+      --                            Markdown
+      -- #####################################################################
 
-    table.insert(
-      markdown_snippets,
-      s({
-        trig = "link",
-        name = "Add this -> []()",
-      }, {
-        t("["),
-        i(1),
-        t("]("),
-        i(2),
-        t(")"),
-      })
-    )
+      -- Helper function to create code block snippets
+      local function create_code_block_snippet(lang)
+         return s(lang, {
+            t({ "```" .. lang, "" }),
+            i(1),
+            t({ "", "```" }),
+         })
+      end
 
-    table.insert(
-      markdown_snippets,
-      s({
-        trig = "linkt",
-        name = 'Add this -> [](){:target="_blank"}',
-      }, {
-        t("["),
-        i(1),
-        t("]("),
-        i(2),
-        t('){:target="_blank"}'),
-      })
-    )
+      -- Define languages for code blocks
+      local languages = {
+         "txt",
+         "lua",
+         "sql",
+         "go",
+         "regex",
+         "bash",
+         "markdown",
+         "markdown_inline",
+         "yaml",
+         "json",
+         "jsonc",
+         "cpp",
+         "csv",
+         "java",
+         "javascript",
+         "python",
+         "dockerfile",
+         "html",
+         "css",
+         "templ",
+         "php",
+      }
 
-    table.insert(
-      markdown_snippets,
-      s({
-        trig = "todo",
-        name = "Add TODO: item",
-      }, {
-        t("<!-- TODO: "),
-        i(1),
-        t(" -->"),
-      })
-    )
+      -- Generate snippets for all languages
+      local markdown_snippets = {}
 
-    -- Paste clipboard contents in link section, move cursor to ()
-    table.insert(
-      markdown_snippets,
-      s({
-        trig = "linkclip",
-        name = "Paste clipboard as .md link",
-      }, {
-        t("["),
-        i(1),
-        t("]("),
-        f(clipboard, {}),
-        t(")"),
-      })
-    )
+      for _, lang in ipairs(languages) do
+         table.insert(markdown_snippets, create_code_block_snippet(lang))
+      end
 
-    ls.add_snippets("markdown", markdown_snippets)
+      table.insert(
+         markdown_snippets,
+         s({
+            trig = "link",
+            name = "Add this -> []()",
+         }, {
+            t("["),
+            i(1),
+            t("]("),
+            i(2),
+            t(")"),
+         })
+      )
 
-    -- #####################################################################
-    --                         all the filetypes
-    -- #####################################################################
-    local all_snippets = {}
+      table.insert(
+         markdown_snippets,
+         s({
+            trig = "linkt",
+            name = 'Add this -> [](){:target="_blank"}',
+         }, {
+            t("["),
+            i(1),
+            t("]("),
+            i(2),
+            t('){:target="_blank"}'),
+         })
+      )
 
-    table.insert(
-      all_snippets,
-      s({
-        trig = "dateiso",
-        name = "Add current date",
-      }, {
-        t(os.date("%Y-%m-%d")),
-      })
-    )
-    table.insert(
-      all_snippets,
-      s({
-        trig = "datelocal",
-        name = "Add current date",
-      }, {
-        t(os.date("%d.%m.%Y")),
-      })
-    )
+      table.insert(
+         markdown_snippets,
+         s({
+            trig = "todo",
+            name = "Add TODO: item",
+         }, {
+            t("<!-- TODO: "),
+            i(1),
+            t(" -->"),
+         })
+      )
 
-    ls.add_snippets("all", all_snippets)
+      -- Paste clipboard contents in link section, move cursor to ()
+      table.insert(
+         markdown_snippets,
+         s({
+            trig = "linkclip",
+            name = "Paste clipboard as .md link",
+         }, {
+            t("["),
+            i(1),
+            t("]("),
+            f(clipboard, {}),
+            t(")"),
+         })
+      )
 
-    return opts
-  end,
+      ls.add_snippets("markdown", markdown_snippets)
+
+      -- #####################################################################
+      --                         all the filetypes
+      -- #####################################################################
+      local all_snippets = {}
+
+      table.insert(
+         all_snippets,
+         s({
+            trig = "dateiso",
+            name = "Add current date",
+         }, {
+            t(os.date("%Y-%m-%d")),
+         })
+      )
+      table.insert(
+         all_snippets,
+         s({
+            trig = "datelocal",
+            name = "Add current date",
+         }, {
+            t(os.date("%d.%m.%Y")),
+         })
+      )
+
+      ls.add_snippets("all", all_snippets)
+
+      return opts
+   end,
 }
