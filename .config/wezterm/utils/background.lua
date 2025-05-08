7f29036022108aa0547ae0efaabf3cd183f90683
Sven Bergner <sven.bergner@gmail.com>
2025-05-08 15:07:24 +0200

    feat(wezterm): Added simply background image instead of animated green rain background

--- .config/wezterm/utils/background.lua
+++ .config/wezterm/utils/background.lua
@@ -2,32 +2,45 @@ local M = {}
 local wezterm = require("wezterm")
 
 M.getFancyBackground = function()
-  local home_dir = wezterm.home_dir
-  return {
-    {
-      source = {
-        File = home_dir .. "/.config/wezterm/background/Matrix-Wallpaper.gif"
-      },
-      width = '100%',
-      hsb = { brightness = .3 },
-    }
-  }
+   local home_dir = wezterm.home_dir
+   return {
+      {
+         source = {
+            File = home_dir .. "/.config/wezterm/background/Matrix-Wallpaper.gif"
+         },
+         width = '100%',
+         hsb = { brightness = .3 },
+      }
+   }
 end
 
 M.getEmptyBackground = function()
-  return {}
+   return {}
+end
+
+M.getSimpleBackground = function()
+   local home_dir = wezterm.home_dir
+   return {
+      {
+         source = {
+            File = home_dir .. "/.config/wezterm/background/neovim3.png"
+         },
+         width = '100%',
+         hsb = { brightness = .3 },
+      }
+   }
 end
 
 M.toggleBackground = function(config)
-  if wezterm.GLOBAL.fancy_background then
-    config.background = M.getFancyBackground()
-    config.window_background_opacity = 1
-    wezterm.GLOBAL.fancy_background = false
-  else
-    config.background = M.getEmptyBackground()
-    config.window_background_opacity = 1
-    wezterm.GLOBAL.fancy_background = true
-  end
+   if wezterm.GLOBAL.fancy_background then
+      config.background = M.getFancyBackground()
+      config.window_background_opacity = 1
+      wezterm.GLOBAL.fancy_background = false
+   else
+      config.background = M.getSimpleBackground()
+      config.window_background_opacity = 1
+      wezterm.GLOBAL.fancy_background = true
+   end
 end
 
 return M
