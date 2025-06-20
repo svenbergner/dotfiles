a493c63fe83c65f097d95cad4443bdee3e5ccec1
Sven Bergner <sven.bergner@gmail.com>
2025-06-20 11:20:32 +0200

    feat(nvim/lsp): Several lsp configs added

---
+++ .config/nvim/lsp/yamlls.lua
@@ -0,0 +1,29 @@
+-- Language Server Protocol configuration for Yaml Language Server
+-- URL: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#yamlls
+
+-- This configuration sets up the Yaml Language Server with specific command, filetypes, and settings.
+vim.lsp.config('yamlls', {
+   settings = {
+      yaml = {
+         schemas = {
+            ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+            ["../path/relative/to/file.yml"] = "/.github/workflows/*",
+            ["/path/from/root/of/project"] = "/.github/workflows/*",
+         },
+      },
+   }
+})
+
+
+return {
+   cmd = { "yaml-language-server", "--stdio" }
+   filetypes = { "yaml" },
+   root_markers = { ".git", },
+   settings = {
+      redhat = {
+         telemetry = {
+            enabled = false
+         }
+      }
+   }
+}
