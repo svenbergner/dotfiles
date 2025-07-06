3f6d75de4a29873ae80648df3aecf8b233223c51
Sven Bergner <sven.bergner@gmail.com>
2025-07-06 15:34:17 +0200

    feat(nvim/vimwiki): Set textwidth to 120 characters

---
+++ .config/nvim/after/ftplugin/vimwiki.lua
@@ -0,0 +1,13 @@
+local group_id = vim.api.nvim_create_augroup("VimWikiSettings", { clear = true })
+
+vim.api.nvim_create_autocmd(
+   { "BufEnter", "BufNewFile", "BufRead", "VimEnter" },
+   {
+   group = group_id,
+   pattern = "*.md",
+   callback = function()
+      vim.cmd('setlocal textwidth=120')
+   end,
+   desc = "Set some settings for vimwiki markdown files",
+})
+
