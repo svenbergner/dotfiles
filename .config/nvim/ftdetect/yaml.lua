5100224905843a6e66f4c6eb3bacc2440066c44d
Sven Bergner <sven.bergner@gmail.com>
2025-07-07 09:20:41 +0200

    chore(nvim): Cleanup yaml filetype detect

--- .config/nvim/ftdetect/yaml.lua
+++ .config/nvim/ftdetect/yaml.lua
@@ -1,17 +1,8 @@
--- Based on this medium article
--- URL: https://itnext.io/better-kubernetes-yaml-editing-with-neo-vim-af7da9a1b150
+-- This file is part of the Neovim configuration for managing YAML files.
 vim.api.nvim_create_autocmd(
-   {'BufRead','BufNewFile'},
+   { 'BufEnter', 'BufRead', 'BufNewFile', 'VimEnter' },
    {
-      pattern = '*.yaml',
-      command = 'set filetype=yaml',
-   }
-)
-
-vim.api.nvim_create_autocmd(
-   {'BufRead','BufNewFile'},
-   {
-      pattern = '*.yml',
+      pattern = { '*.yaml', '*.yml' },
       command = 'set filetype=yaml',
    }
 )
