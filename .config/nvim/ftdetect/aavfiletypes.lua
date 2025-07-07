4f347607b05c4c17eb27d0b09d2a1068924d571e
Sven Bergner <sven.bergner@gmail.com>
2025-07-07 13:52:11 +0200

    feat(nvim): More events added

--- .config/nvim/ftdetect/aavfiletypes.lua
+++ .config/nvim/ftdetect/aavfiletypes.lua
@@ -1,7 +1,7 @@
 -- Autocommands for setting filetypes for files with specific extensions
 
 vim.api.nvim_create_autocmd(
-   {'BufRead','BufNewFile'},
+   { 'BufEnter', 'BufRead', 'BufNewFile', 'VimEnter' },
    {
       pattern = {'*.htd', '*.htd.inc'},
       command = 'set filetype=html',
