6591364394ddf4c223aa5f06a5c1d58024015d1a
Sven Bergner <sven.bergner@gmail.com>
2025-08-09 13:40:25 +0200

    feat(nvim): Smear cursor plugin added

---
+++ .config/nvim/lua/plugins/smear_cursor.lua
@@ -0,0 +1,16 @@
+--[===[
+Smear cursor for Neovim
+Neovim plugin to animate the cursor with a smear effect in all terminals.
+Inspired by Neovide's animated cursor.
+
+This plugin is intended for terminals/GUIs that can only display text and do 
+not have graphical capabilities (unlike Neovide, or the Kitty terminal). 
+
+URL: https://github.com/sphamba/smear-cursor.nvim
+--]===]
+
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {},
+}
+
