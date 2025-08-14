53f40d4cb0d9b8c09b193c2493d393a6b144c1f9
Sven Bergner <sven.bergner@gmail.com>
2025-08-14 16:26:08 +0200

    feat(nvim): Added typst support

---
+++ .config/nvim/lua/plugins/typst-preview.lua
@@ -0,0 +1,18 @@
+--[===[
+Typst Preview for Neovim
+
+💪 Features 
+- Low latency preview: preview your document instantly on type. The
+incremental rendering technique makes the preview latency as low as possible.
+- Cross jump between code and preview. You can click on the preview to jump to the
+corresponding code location and have the preview follow your cursor in Neovim.
+
+URL: https://github.com/chomosuke/typst-preview.nvim
+--]===]
+
+return {
+   'chomosuke/typst-preview.nvim',
+   lazy = false, -- or ft = 'typst'
+   version = '1.*',
+   opts = {},   -- lazy.nvim will implicitly calls `setup {}`
+}
