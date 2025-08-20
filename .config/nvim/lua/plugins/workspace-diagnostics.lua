99a070d1e56f63c2a80eb1c5c9fa19af7e88ed46
Sven Bergner <sven.bergner@gmail.com>
2025-08-20 10:18:03 +0200

    feat(nvim): Added plugin to show projectwide diagnostics

---
+++ .config/nvim/lua/plugins/workspace-diagnostics.lua
@@ -0,0 +1,12 @@
+--[===[
+Workspace-diagnostics.nvim
+Populates project-wide lsp diagnostics, regardless of what files are opened.
+
+URL: https://github.com/artemave/workspace-diagnostics.nvim
+--]===]
+
+return {
+   "artemave/workspace-diagnostics.nvim",
+   enabled = true,
+   config = {}
+}
