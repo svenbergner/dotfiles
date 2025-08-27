b0fe91073ebba3cdf503a952c8ed7a11288a1177
Sven Bergner <sven.bergner@gmail.com>
2025-08-27 20:00:59 +0200

    chore(nvim): Code Style

--- .config/wezterm/layouts/development.lua
+++ .config/wezterm/layouts/development.lua
@@ -1,43 +1,42 @@
 local M = {}
 
-M.addTab = function(window, cwd, title, text_to_send)
-  local tab, pane, _ = window:spawn_tab {
-    cwd = cwd,
-  }
-  tab:set_title(title)
-  if #text_to_send > 0 then
-    pane:send_text(text_to_send)
-  end
-  return tab
+M.addTab = function(window, cwd, title, command)
+   local tab, pane, _ = window:spawn_tab {
+      cwd = cwd,
+   }
+   tab:set_title(title)
+   if #command > 0 then
+      pane:send_text(command)
+   end
+   return tab
 end
 
 M.setup_layout = function(wezterm)
-  local mux = wezterm.mux
+   local mux = wezterm.mux
 
-  wezterm.on('gui-startup', function()
-    local home_dir = wezterm.home_dir
-    if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-      home_dir = '/C'
-    end
+   wezterm.on('gui-startup', function()
+      local home_dir = wezterm.home_dir
+      if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+         home_dir = '/C'
+      end
 
-    local first_tab, first_pane, window = mux.spawn_window {
-      workspace = 'Development',
-      cwd = home_dir .. '/Repos/steuertipps_app',
-    }
-    first_tab:set_title('SSECloud')
-    first_pane:send_text('nvim\n')
+      local first_tab, first_pane, window = mux.spawn_window {
+         workspace = 'Development',
+         cwd = home_dir .. '/Repos/steuertipps_app',
+      }
+      first_tab:set_title('SSECloud')
+      first_pane:send_text('nvim\n')
 
-    local activatedTab = M.addTab(window, home_dir .. '/Repos/SSE/Dev', 'SSE/TaxCoreApi', 'nvim\n')
-    M.addTab(window, home_dir .. '/Repos/Content/StP/30', 'Content StP', 'nvim\n')
-    M.addTab(window, home_dir .. '/Repos/SSE/Tools', 'Tools', 'nvim\n')
-    M.addTab(window, home_dir .. '/Repos/DevTools', 'DevTools', 'nvim\n')
-    M.addTab(window, home_dir .. '/Repos/SetupSSE', 'SetupSSE', 'nvim\n')
-    M.addTab(window, home_dir .. '/Repos/BuildAgentIAC', 'BuildAgentIAC', 'nvim\n')
+      local activatedTab = M.addTab(window, home_dir .. '/Repos/SSE/Dev', 'SSE/TaxCoreApi', 'nvim\n')
+      M.addTab(window, home_dir .. '/Repos/Content/StP/30', 'Content StP', 'nvim\n')
+      M.addTab(window, home_dir .. '/Repos/SSE/Tools', 'Tools', 'nvim\n')
+      M.addTab(window, home_dir .. '/Repos/DevTools', 'DevTools', 'nvim\n')
+      M.addTab(window, home_dir .. '/Repos/SetupSSE', 'SetupSSE', 'nvim\n')
+      M.addTab(window, home_dir .. '/Repos/BuildAgentIAC', 'BuildAgentIAC', 'nvim\n')
 
-    activatedTab:activate()
-    window:gui_window():maximize()
-
-  end)
+      activatedTab:activate()
+      window:gui_window():maximize()
+   end)
 end
 
 return M
