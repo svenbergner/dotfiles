233e4fd17c2c532ff9ccac4fb4313ab4eb9150cd
Sven Bergner <sven.bergner@gmail.com>
2025-10-08 14:03:46 +0200

    feat(nvim/action-preview): Added delta as a diff viewer

--- .config/nvim/lua/plugins/actions-preview.lua
+++ .config/nvim/lua/plugins/actions-preview.lua
@@ -9,5 +9,39 @@ return {
    enabled = true,
    config = function()
       vim.keymap.set({ "v", "n" }, "ca", require("actions-preview").code_actions, { desc = "Preview [c]ode [a]ctions" })
+
+      require("actions-preview").setup {
+         telescope = {
+            sorting_strategy = "ascending",
+            layout_strategy = "vertical",
+            layout_config = {
+               width = 0.7,
+               height = 0.8,
+               prompt_position = "top",
+               preview_cutoff = 20,
+               preview_height = function(_, _, max_lines)
+                  return max_lines - 15
+               end,
+            },
+         },
+      }
+
+      local hl = require("actions-preview.highlight")
+      require("actions-preview").setup {
+         highlight_command = {
+            -- Highlight diff using delta: https://github.com/dandavison/delta
+            -- The argument is optional, in which case "delta" is assumed to be specified.
+            -- hl.delta("path/to/delta --option1 --option2"),
+            -- You may need to specify "--no-gitconfig" since it is dependent on
+            -- the gitconfig of the project by default.
+            hl.delta("delta --no-gitconfig --side-by-side"),
+
+            -- Functions can also be specified for items. Functions are executed during setup.
+            -- This is useful for `require(...)` at definition time, such as in lazy.nvim.
+            function()
+               return require("actions-preview.highlight").delta()
+            end,
+         },
+      }
    end
 }
