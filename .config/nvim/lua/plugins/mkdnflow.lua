43b7625f6de3c1588e7c12a10b1be7d8d37b2fa9
Sven Bergner <sven.bergner@gmail.com>
2025-10-13 13:59:25 +0200

    feat(nvim/mkdnflow): Added custom keymaps

--- .config/nvim/lua/plugins/mkdnflow.lua
+++ .config/nvim/lua/plugins/mkdnflow.lua
@@ -1,5 +1,5 @@
 --[===[
-Mkdnflow 
+Mkdnflow
 
 Description:
 
@@ -35,7 +35,43 @@ return {
    enabled = false,
    config = function()
       require('mkdnflow').setup({
-         -- Config goes here; leave blank for defaults
+         mappings = {
+            MkdnEnter = { { 'n', 'v' }, '<CR>' },
+            MkdnTab = false,
+            MkdnSTab = false,
+            MkdnNextLink = { 'n', '<Tab>' },
+            MkdnPrevLink = { 'n', '<S-Tab>' },
+            MkdnNextHeading = { 'n', ']]' },
+            MkdnPrevHeading = { 'n', '[[' },
+            MkdnGoBack = { 'n', '<BS>' },
+            MkdnGoForward = { 'n', '<Del>' },
+            MkdnCreateLink = false,                                      -- see MkdnEnter
+            MkdnCreateLinkFromClipboard = { { 'n', 'v' }, '<leader>p' }, -- see MkdnEnter
+            MkdnFollowLink = false,                                      -- see MkdnEnter
+            MkdnDestroyLink = { 'n', '<M-CR>' },
+            MkdnTagSpan = { 'v', '<M-CR>' },
+            MkdnMoveSource = { 'n', '<F2>' },
+            MkdnYankAnchorLink = { 'n', 'yaa' },
+            MkdnYankFileAnchorLink = { 'n', 'yfa' },
+            MkdnIncreaseHeading = { 'n', '+' },
+            MkdnDecreaseHeading = { 'n', '-' },
+            MkdnToggleToDo = { { 'n', 'v' }, '<C-Space>' },
+            MkdnNewListItem = false,
+            MkdnNewListItemBelowInsert = { 'n', 'o' },
+            MkdnNewListItemAboveInsert = { 'n', 'O' },
+            MkdnExtendList = false,
+            MkdnUpdateNumbering = { 'n', '<leader>mun' },
+            MkdnTableNextCell = { 'i', '<Tab>' },
+            MkdnTablePrevCell = { 'i', '<S-Tab>' },
+            MkdnTableNextRow = false,
+            MkdnTablePrevRow = { 'i', '<M-CR>' },
+            MkdnTableNewRowBelow = { 'n', '<leader>ir' },
+            MkdnTableNewRowAbove = { 'n', '<leader>iR' },
+            MkdnTableNewColAfter = { 'n', '<leader>ic' },
+            MkdnTableNewColBefore = { 'n', '<leader>iC' },
+            MkdnFoldSection = { 'n', '<leader>f' },
+            MkdnUnfoldSection = { 'n', '<leader>F' }
+         }
       })
    end
 }
