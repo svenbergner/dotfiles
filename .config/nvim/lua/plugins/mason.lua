451b9aeb68875ed36259029a06a7d49e63b9d02a
Sven Bergner <sven.bergner@gmail.com>
2025-10-22 11:41:57 +0200

    refactor(nvim/lsp): Better nvim 11.x conform usage of lsp's

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,37 +1,77 @@
 --[===[
+Mason.nvim configuration for Neovim
+Provides a portable package manager for Neovim that allows installing and managing
+LSP servers, DAP servers, linters, and formatters.
 
 URL: https://github.com/mason-org/mason.nvim
+URL: https://github.com/mason-org/mason-lspconfig.nvim
 URL: https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
+URL: https://github.com/neovim/nvim-lspconfig
 URL: https://github.com/j-hui/fidget.nvim
 URL: https://github.com/saghen/blink.cmp
 --]===]
 
 return {
-   'mason-org/mason.nvim',
+   'mason-org/mason-lspconfig.nvim',
    enabled = true,
+   opts = {
+      ensure_installed = {
+         -- LSPs
+         'azure_pipelines_ls',
+         'bashls',
+         'clangd',
+         'cmake',
+         -- 'codelldb',
+         'jsonls',
+         'lua_ls',
+         'marksman',
+         'pico8_ls',
+         'pylsp',
+         -- 'sonarlint-language-server',
+         'spectral',
+         'terraformls',
+         'tinymist',
+         'yamlls',
+      },
+   },
    dependencies = {
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
+      {
+         'mason-org/mason.nvim',
+         opts = {
+            ui = {
+               border = 'rounded',
+               icons = {
+                  package_installed = "✓",
+                  package_pending = "➜",
+                  package_uninstalled = "✗",
+               }
+            },
+         },
+      },
+      'neovim/nvim-lspconfig',
+      {
+         'WhoIsSethDaniel/mason-tool-installer.nvim',
+         opts = {
+            ensure_installed = {
+               -- DAPs
+               'bash-debug-adapter',
+               'dart-debug-adapter',
+
+               -- Formatters and Linters
+               'clang-format',
+               'cmakelint',
+               'flake8',
+               'hclfmt',
+               'markdown-toc',
+               'pyright',
+               'stylua',
+               'terraform',
+            },
+         },
+      },
       -- Useful status updates for LSP.
       { 'j-hui/fidget.nvim', opts = {} },
       -- Allows extra capabilities provided by blink-cmp
       'saghen/blink.cmp',
    },
-   opts = {
-      ui = {
-         icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-         }
-      },
-   },
-   config = function(_, opts)
-      require('mason').setup(opts)
-      require('mason-tool-installer').setup({
-         ensure_installed = opts.ensure_installed,
-         auto_update = true,
-         run_on_start = true,
-         run_on_change = true,
-      })
-   end,
 }
