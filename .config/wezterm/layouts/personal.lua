531a948a5763ab2cead3a3295e7d5eddd34ced75
Sven Bergner <sven.bergner@gmail.com>
2025-10-28 13:48:12 +0100

    fix(wezterm): Don't start neovim in personal tabs

--- .config/wezterm/layouts/personal.lua
+++ .config/wezterm/layouts/personal.lua
@@ -23,13 +23,13 @@ M.setup_layout = function(wezterm)
          cwd = home_dir .. '/Repos/dotfiles/',
       }
       first_tab:set_title('Dotfiles')
-      first_pane:send_text('nvim\n')
+      first_pane:send_text('')
 
-      M.addTab(window, home_dir .. '/Repos/vimwiki', 'VimWiki', 'nvim\n')
-      M.addTab(window, home_dir .. '/Repos/telescope-cmake-preset-selector', 'CMake Preset Selector', 'nvim\n')
-      M.addTab(window, home_dir .. '/Repos/telescope-debugee-selector', 'Debugee Selector', 'nvim\n')
-      -- M.addTab(window, home_dir .. '/Repos/snacks.nvim', 'Snacks', 'nvim\n')
-      -- M.addTab(window, home_dir .. '/Repos/presenter.nvim', 'Presenter', 'nvim\n')
+      M.addTab(window, home_dir .. '/Repos/vimwiki', 'VimWiki', '')
+      M.addTab(window, home_dir .. '/Repos/telescope-cmake-preset-selector', 'CMake Preset Selector', '')
+      M.addTab(window, home_dir .. '/Repos/telescope-debugee-selector', 'Debugee Selector', '')
+      -- M.addTab(window, home_dir .. '/Repos/snacks.nvim', 'Snacks', '')
+      -- M.addTab(window, home_dir .. '/Repos/presenter.nvim', 'Presenter', '')
 
       first_tab:activate()
    end)
