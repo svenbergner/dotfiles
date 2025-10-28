2eaeb80737df98253f79a06d3b9226b548b9a86a
Sven Bergner <sven.bergner@gmail.com>
2025-10-28 14:14:26 +0100

    chore(nvim): Refactoring Coding Style

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -28,52 +28,54 @@ return {
       local neotest = require('neotest')
       neotest.setup({
          adapters = {
-            require('neotest-dart') {
+            require('neotest-dart')({
                command = 'fvm flutter',
                use_lsp = true,
                -- Useful when using custom test names with @isTest annotation
                custom_test_method_names = {},
-            },
-            require('neotest-python') {
+            }),
+            require('neotest-python')({
                runner = 'pytest',
                python = 'python',
                use_lsp = true,
-            },
-            require('neotest-plenary') {},
+            }),
+            require('neotest-plenary')({}),
             require('neotest-ctest').setup({
                is_test_file = function(file_path)
-                  return file_path:match('test_.*%.c$') ~= nil or file_path:match('.*_test%.c$') ~= nil or
-                      file_path:match('test_.*%.cpp$') ~= nil or file_path:match('.*Test%.cpp$') ~= nil
+                  return file_path:match('test_.*%.c$') ~= nil
+                     or file_path:match('.*_test%.c$') ~= nil
+                     or file_path:match('test_.*%.cpp$') ~= nil
+                     or file_path:match('.*Test%.cpp$') ~= nil
                end,
             }),
             -- require('neotest-catch2') {},
-         }
+         },
       })
 
       vim.keymap.set('n', '<leader>TN', function()
          vim.cmd('wa')
          neotest.run.run()
-      end, { desc = '[T]est: run [N]earest' });
+      end, { desc = '[T]est: run [N]earest' })
 
       vim.keymap.set('n', '<leader>TS', function()
          neotest.run.stop()
-      end, { desc = '[T]est: [s]top running test' });
+      end, { desc = '[T]est: [s]top running test' })
 
       vim.keymap.set('n', '<leader>TA', function()
          vim.cmd('wa')
          neotest.run.run(vim.fn.expand('%'))
-      end, { desc = '[T]ests run [A]ll in file' });
+      end, { desc = '[T]ests run [A]ll in file' })
 
       vim.keymap.set('n', '<leader>TD', function()
          neotest.run.run({ strategy = 'dap' })
-      end, { desc = '[T]est: [D]ebug nearest test' });
+      end, { desc = '[T]est: [D]ebug nearest test' })
 
       vim.keymap.set('n', '<leader>TR', function()
          neotest.output.open({ enter = true, quiet = true, auto_close = true, last_run = true })
-      end, { desc = 'Show [T]est [R]esults' });
+      end, { desc = 'Show [T]est [R]esults' })
 
       vim.keymap.set('n', '<leader>TT', function()
          neotest.summary.toggle()
-      end, { desc = '[T]oggle [T]est summary' });
+      end, { desc = '[T]oggle [T]est summary' })
    end,
 }
