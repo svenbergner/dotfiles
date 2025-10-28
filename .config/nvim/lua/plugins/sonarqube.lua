dcbc7b84626ca3c95816d81822eaf1794a4cdac0
Sven Bergner <sven.bergner@gmail.com>
2025-10-28 13:56:19 +0100

    chore(nvim): Refactoring Coding Style

--- .config/nvim/lua/plugins/sonarqube.lua
+++ .config/nvim/lua/plugins/sonarqube.lua
@@ -6,55 +6,55 @@ static analysis and code actions directly in neovim.
 URL: https://gitlab.com/schrieveslaach/sonarlint.nvim
 --]===]
 
-local analyzers_path = vim.fn.stdpath "data" .. "/mason/packages/sonarlint-language-server/extension/analyzers/"
+local analyzers_path = vim.fn.stdpath 'data' .. '/mason/packages/sonarlint-language-server/extension/analyzers/'
 
 local sonarlint_ft = {
-   "c",
-   "cpp",
-   "css",
-   "docker",
-   "go",
-   "html",
-   "java",
-   "javascript",
-   "javascriptreact",
-   "php",
-   "python",
-   "typescript",
-   "typescriptreact",
-   "xml",
-   "yaml.docker-compose",
+   'c',
+   'cpp',
+   'css',
+   'docker',
+   'go',
+   'html',
+   'java',
+   'javascript',
+   'javascriptreact',
+   'php',
+   'python',
+   'typescript',
+   'typescriptreact',
+   'xml',
+   'yaml.docker-compose',
 }
 
 return {
-   "https://gitlab.com/schrieveslaach/sonarlint.nvim",
+   'https://gitlab.com/schrieveslaach/sonarlint.nvim',
    enabled = true,
    ft = sonarlint_ft,
    opts = {
       connected = {
          get_credentials = function(_, _)
-            return vim.fn.getenv("SONAR_TOKEN")
+            return vim.fn.getenv('SONAR_TOKEN')
          end,
       },
       server = {
          cmd = {
-            "sonarlint-language-server",
-            "-stdio",
-            "-analyzers",
-            analyzers_path .. "sonargo.jar",      -- Go
-            analyzers_path .. "sonarcfamily.jar", -- C, C++
-            analyzers_path .. "sonarhtml.jar",    -- HTML
-            analyzers_path .. "sonariac.jar",     -- Infrastructure-as-Code
-            analyzers_path .. "sonarjs.jar",      -- JavaScript, TypeScript
-            analyzers_path .. "sonarpython.jar",  -- Python
-            analyzers_path .. "sonarxml.jar",     -- XML, XSLT
+            'sonarlint-language-server',
+            '-stdio',
+            '-analyzers',
+            analyzers_path .. 'sonargo.jar',      -- Go
+            analyzers_path .. 'sonarcfamily.jar', -- C, C++
+            analyzers_path .. 'sonarhtml.jar',    -- HTML
+            analyzers_path .. 'sonariac.jar',     -- Infrastructure-as-Code
+            analyzers_path .. 'sonarjs.jar',      -- JavaScript, TypeScript
+            analyzers_path .. 'sonarpython.jar',  -- Python
+            analyzers_path .. 'sonarxml.jar',     -- XML, XSLT
             -- analyzers_path .. "sonarjava.jar",
             -- analyzers_path .. "sonarjavasymbolicexecution.jar",
             -- analyzers_path .. "sonarphp.jar",
          },
          settings = {
             sonarlint = {
-               pathToCompileCommands = analyzers_path .. "compile_commands.json",
+               pathToCompileCommands = analyzers_path .. 'compile_commands.json',
                rules = {
                   -- Disable some rules that are not useful in our context
                   -- or that produce too many false positives.
@@ -62,22 +62,22 @@ return {
                   -- https://sonarsource.github.io/rspec/#/rspec/SXXXX
                   -- or search for a rule by name
                   -- https://sonarsource.github.io/rspec/#/rspec/?lang=cfamily&query=replace+new
-                  ["cpp:S125"]  = { level = 'off' }, -- Sections of code should not be commented out
-                  ["cpp:S134"]  = { level = 'off' }, -- Control flow statements "IF", "CASE", "DO", "LOOP", "SELECT", "WHILE" and "PROVIDE" should not be nested too deeply
-                  ["cpp:S995"]  = { level = 'off' }, -- Change to pointer-to-const
-                  ["cpp:S3471"] = { level = 'off' }, -- "override" or "final" should be used instead of "virtual"
-                  ["cpp:S3576"] = { level = 'off' }, -- "final" classes should not have "virtual" functions
-                  ["cpp:S5025"] = { level = 'off' }, -- Memory should not be managed manually
-                  ["cpp:S5350"] = { level = 'off' }, -- Pointer and reference local variables should be "const" if the corresponding object is not modified
-                  ["cpp:S6004"] = { level = 'off' }, -- "if" and "switch" initializer should be used to reduce scope of variables
+                  ['cpp:S125']  = { level = 'off' }, -- Sections of code should not be commented out
+                  ['cpp:S134']  = { level = 'off' }, -- Control flow statements "IF", "CASE", "DO", "LOOP", "SELECT", "WHILE" and "PROVIDE" should not be nested too deeply
+                  ['cpp:S995']  = { level = 'off' }, -- Change to pointer-to-const
+                  ['cpp:S3471'] = { level = 'off' }, -- "override" or "final" should be used instead of "virtual"
+                  ['cpp:S3576'] = { level = 'off' }, -- "final" classes should not have "virtual" functions
+                  ['cpp:S5025'] = { level = 'off' }, -- Memory should not be managed manually
+                  ['cpp:S5350'] = { level = 'off' }, -- Pointer and reference local variables should be "const" if the corresponding object is not modified
+                  ['cpp:S6004'] = { level = 'off' }, -- "if" and "switch" initializer should be used to reduce scope of variables
                },
                connectedMode = {
                   connections = {
                      sonarqube = {
                         {
-                           connectionId = "https-sonarqube-cloud-dev-wolterskluwer-eu-",
+                           connectionId = 'https-sonarqube-cloud-dev-wolterskluwer-eu-',
                            -- this is the url that will go into get_credentials
-                           serverUrl = "https://sonarqube.cloud-dev.wolterskluwer.eu/",
+                           serverUrl = 'https://sonarqube.cloud-dev.wolterskluwer.eu/',
                            disableNotifications = false,
                         },
                      },
@@ -94,12 +94,12 @@ return {
             -- https://github.com/folke/neoconf.nvim or some similar
             -- plugin, might be worthwhile.
             local project_root_and_ids = {
-               ["~/Repos/SSE/Dev/"] = "TAA.DE.Steuertipps.SSE",
+               ['~/Repos/SSE/Dev/'] = 'TAA.DE.Steuertipps.SSE',
                -- … further mappings …
             }
 
             config.settings.sonarlint.connectedMode.project = {
-               connectionId = "https-sonarqube-cloud-dev-wolterskluwer-eu-",
+               connectionId = 'https-sonarqube-cloud-dev-wolterskluwer-eu-',
                projectKey = project_root_and_ids[params.rootPath],
             }
          end,
