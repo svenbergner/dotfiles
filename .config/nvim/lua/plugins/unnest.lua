126445acd97c435aa64074f360bd099132ac9e8f
Sven Bergner <sven.bergner@gmail.com>
2025-10-29 15:13:12 +0100

    feat(nvim): Added unnest package

---
+++ .config/nvim/lua/plugins/unnest.lua
@@ -0,0 +1,20 @@
+--[===[
+unnest.nvim
+Unnest your nested Neovim sessions.
+
+Introduction
+Imagine that you are in a terminal buffer in Nvim, and you run a command that 
+opens a new Nvim instance (e.g., git commit). Now you have a Neovim session 
+running inside another Neovim session. This can be confusing and inefficient.
+
+Solves this by detecting when it's being run in a nested session, then it will 
+instruct the parent Neovim instance to open files in a in the parent Neovim instance.
+
+URL: https://github.com/brianhuster/unnest.nvim
+--]===]
+
+return {
+   'brianhuster/unnest.nvim',
+   enabled = true,
+}
+
