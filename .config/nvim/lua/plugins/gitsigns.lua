66158c43ed1938f58a56d0d201dd97ae190b56af
Sven Bergner <sven.bergner@gmail.com>
2025-11-04 13:35:32 +0100

    chore(nvim): Coding style
    chore(nvim): Coding style

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -5,19 +5,19 @@ URL: https://github.com/lewis6991/gitsigns.nvim
 
 ---@diagnostic disable: param-type-mismatch
 return {
-   "lewis6991/gitsigns.nvim",
+   'lewis6991/gitsigns.nvim',
    enabled = true,
    event = 'VeryLazy',
    config = function()
-      require("gitsigns").setup({
+      require('gitsigns').setup({
          -- See `:help gitsigns.txt`
          signs = {
-            add = { text = "" },
+            add = { text = '' },
 
-            change = { text = "" },
-            delete = { text = "_" },
-            topdelete = { text = "‾" },
-            changedelete = { text = "" },
+            change = { text = '' },
+            delete = { text = '_' },
+            topdelete = { text = '‾' },
+            changedelete = { text = '' },
          },
          numhl = true, -- Change color of line number
       })
