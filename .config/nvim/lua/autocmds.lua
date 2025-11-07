f0124b874d16a485ebb52a5e79674bd0ae6da280
Sven Bergner <sven.bergner@gmail.com>
2025-11-07 14:45:40 +0100

    feat(nvim): Restore cursor postion after loading a file

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -11,6 +11,21 @@ autocmd('TextYankPost', {
    end,
 })
 
+-- restore cursor to file position in previous editing session
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function(args)
+		local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+		local line_count = vim.api.nvim_buf_line_count(args.buf)
+		if mark[1] > 0 and mark[1] <= line_count then
+			vim.api.nvim_win_set_cursor(0, mark)
+			-- defer centering slightly so it's applied after render
+			vim.schedule(function()
+				vim.cmd("normal! zz")
+			end)
+		end
+	end,
+})
+
 -- Toggle relative line numbers based on focus
 local numberToggleGroup = augroup("NumberToggle", { clear = true })
 autocmd({ "BufEnter", "FocusGained", "InsertLeave", "WinEnter" }, {
