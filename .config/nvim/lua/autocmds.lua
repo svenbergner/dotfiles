a81a0056c9f9df126d079c8682a2175064478920
Sven Bergner <sven.bergner@gmail.com>
2025-11-07 14:46:31 +0100

    feat(nvim): Show cursorline only in active window

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -66,3 +66,19 @@ autocmd({ "CursorMoved" }, {
       end
    end,
 })
+
+-- show cursorline only in active window enable
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+	group = vim.api.nvim_create_augroup("active_cursorline", { clear = true }),
+	callback = function()
+		vim.opt_local.cursorline = true
+	end,
+})
+
+-- show cursorline only in active window disable
+vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
+	group = "active_cursorline",
+	callback = function()
+		vim.opt_local.cursorline = false
+	end,
+})
