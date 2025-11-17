8700137da2aca4ef7f931cdfb9c29050c7c7ed2a
Sven Bergner <sven.bergner@gmail.com>
2025-11-17 10:33:37 +0100

    feat(nvim): Give the actions-preview a little more space

--- .config/nvim/lua/plugins/actions-preview.lua
+++ .config/nvim/lua/plugins/actions-preview.lua
@@ -15,8 +15,8 @@ return {
             sorting_strategy = "ascending",
             layout_strategy = "vertical",
             layout_config = {
-               width = 0.7,
-               height = 0.8,
+               width = 0.8,
+               height = 0.9,
                prompt_position = "top",
                preview_cutoff = 20,
                preview_height = function(_, _, max_lines)
