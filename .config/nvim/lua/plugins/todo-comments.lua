4f247bdcb0e93a410705d20454a906b6fbda52c8
Sven Bergner <sven.bergner@gmail.com>
2025-11-25 12:56:28 +0100

    feat(nvim): Added icons for NOTE, TEST and PROJECT comments

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -1,4 +1,4 @@
---[===[ 
+--[===[
 Highlight todo, notes, etc in comments
 TODO:
 OPTIMIZE:
@@ -10,7 +10,7 @@ BUG:
 WARN:
 HACK:
 NOTE:
-INFO: 
+INFO:
 
 URL: https://www.github.com/folke/todo-comments.nvim
 --]===]
@@ -25,10 +25,11 @@ return {
       signs = true,
       keywords = {
          FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
-         PROJECT = { icon = "  ", color = "info", alt = { "PROJECT" } },
+         PROJECT = { icon = " ", color = "info", alt = { "PROJECT" } },
          TODO = { icon = " ", color = "info", alt = { "TODO" } },
+         TEST = { icon = "󰙨 ", color = "info" },
          WARN = { icon = " ", color = "warning", alt = { "WARN", "WARNING" } },
-         NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+         NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
       },
    },
 }
