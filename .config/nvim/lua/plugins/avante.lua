e2c9e271469351a98a55b9c7214d747db2125ed4
Sven Bergner <sven.bergner@gmail.com>
2026-01-08 18:48:00 +0100

    chore(nvim/avante): Deaktivated avante auto suggestions because they conflict with blink

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -31,7 +31,7 @@ return {
       },
       auto_suggestions_provider = "copilot",
       behaviour = {
-         auto_suggestions = true, -- Experimental stage
+         auto_suggestions = false, -- Experimental stage
          auto_set_highlight_group = true,
          auto_set_keymaps = true,
          auto_apply_diff_after_generation = false,
