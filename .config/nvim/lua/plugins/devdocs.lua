fa277cb9257267f8c212db4d69aa524d5b086c52
Sven Bergner <sven.bergner@gmail.com>
2026-01-09 14:17:52 +0100

    feat(nvim): DevDocs plugin added

---
+++ .config/nvim/lua/plugins/devdocs.lua
@@ -0,0 +1,15 @@
+--[===[
+Nvim Devdocs Plugin
+
+URL: https://github.com/luckasRanarison/nvim-devdocs
+--]===]
+
+return {
+  "luckasRanarison/nvim-devdocs",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {}
+}
