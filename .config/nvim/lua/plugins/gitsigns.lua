accace844d447ff5ba5a2b187a3021de1a63b4ed
Sven Bergner <sven.bergner@gmail.com>
2026-01-11 20:48:13 +0100

    fix(nvim/gitsigns): Changed position for better readabilty

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -25,7 +25,7 @@ return {
          current_line_blame_formatter_nc = '  Not Committed Yet',
          current_line_blame_opts = {
             virt_text = true,
-            virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
+            virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
             delay = 0,
             ignore_whitespace = false,
             virt_text_priority = 100,
