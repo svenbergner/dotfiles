e27382715a37b18241baee505857b1bdca64acce
Sven Bergner <sven.bergner@gmail.com>
2026-01-15 13:14:33 +0100

    feat(nvim/dap): Use the same dap config if a objcpp or cmake file is open

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -147,6 +147,10 @@ return {
             -- },
          }
 
+         -- Treat CMake and Objective-C the same as C++
+         dap.configurations.objcpp = dap.configurations.cpp
+         dap.configurations.cmake = dap.configurations.cpp
+
          dap.adapters.dart = {
             type = 'executable',
             command = 'dart',
