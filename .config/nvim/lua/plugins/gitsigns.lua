e49f05bbfbd71b32d31613e90edc4ef12b1fc526
Sven Bergner <sven.bergner@gmail.com>
2026-01-28 19:53:51 +0100

    refactor(nvim): avoid require calls

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -13,7 +13,8 @@ return {
    enabled = true,
    event = 'VeryLazy',
    config = function()
-      require('gitsigns').setup({
+      local gitsigns = require('gitsigns')
+      gitsigns.setup({
          -- See `:help gitsigns.txt`
          signs = {
             add = { text = '' },
@@ -41,31 +42,37 @@ return {
          },
       })
       vim.keymap.set('n', '<leader>gb', function()
-         require('gitsigns').blame_line({ full = true })
+         gitsigns.blame_line({ full = true })
       end, { desc = '[g]it [b]lame current line' })
       vim.keymap.set('n', '<leader>gB', function()
-         require('gitsigns').blame()
+         gitsigns.blame()
       end, { desc = '[g]it [B]lame current buffer' })
-      vim.keymap.set('n', '<leader>gD', require('gitsigns').diffthis, { desc = '[g]it [D]iff this' })
-      vim.keymap.set('n', '<leader>gh', require('gitsigns').preview_hunk, { desc = '[g]it preview [h]unk' })
+      vim.keymap.set('n', '<leader>gD', gitsigns.diffthis, { desc = '[g]it [D]iff this' })
+      vim.keymap.set('n', '<leader>gh', gitsigns.preview_hunk, { desc = '[g]it preview [h]unk' })
       vim.keymap.set('n', '<leader>gn', function()
-         require('gitsigns').nav_hunk('next')
+         gitsigns.nav_hunk('next')
       end, { desc = '[g]it Hunk [n]ext' })
       vim.keymap.set('n', '<leader>gN', function()
-         require('gitsigns').nav_hunk('prev')
+         gitsigns.nav_hunk('prev')
       end, { desc = '[g]it hu[N]k previous' })
-      vim.keymap.set('n', '<leader>gr', require('gitsigns').reset_hunk, { desc = '[g]it [r]eset hunk' })
+      vim.keymap.set('n', '<leader>gr', gitsigns.reset_hunk, { desc = '[g]it [r]eset hunk' })
       vim.keymap.set('n', '<leader>gR', require('gitsigns').reset_buffer, { desc = '[g]it [R]eset buffer' })
       vim.keymap.set('n', '<leader>gs', require('gitsigns').stage_hunk, { desc = '[g]it [s]tage or unstage hunk' })
       vim.keymap.set('n', '<leader>gS', require('gitsigns').stage_buffer, { desc = '[g]it [S]tage current buffer' })
+      vim.keymap.set('n', '<leader>gR', gitsigns.reset_buffer, { desc = '[g]it [R]eset buffer' })
+      vim.keymap.set('n', '<leader>gs', gitsigns.stage_hunk, { desc = '[g]it [s]tage or unstage hunk' })
+      vim.keymap.set('n', '<leader>gS', gitsigns.stage_buffer, { desc = '[g]it [S]tage current buffer' })
       vim.keymap.set(
          'n',
          '<leader>gt',
-         require('gitsigns').toggle_current_line_blame,
+         gitsigns.toggle_current_line_blame,
          { desc = '[g]it [t]oggle current line blame' }
       )
       vim.keymap.set('n', '<leader>gt', require('gitsigns').preview_hunk_inline, { desc = '[g]it [T]oggle deleted' })
       vim.keymap.set('n', '<leader>gw', require('gitsigns').toggle_word_diff, { desc = '[g]it toggle [w]ord diff' })
+      vim.keymap.set('n', '<leader>gt', gitsigns.preview_hunk_inline, { desc = '[g]it [T]oggle deleted' })
+      vim.keymap.set('n', '<leader>gv', gitsigns.select_hunk, { desc = '[g]it [v]isual select current hunk' })
+      vim.keymap.set('n', '<leader>gw', gitsigns.toggle_word_diff, { desc = '[g]it toggle [w]ord diff' })
 
       -- Remove last search highlight after cursor moved
       local autocmd = vim.api.nvim_create_autocmd
