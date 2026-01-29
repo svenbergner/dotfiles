ea88233559ab0e2abd1362600cbef3afe418c29f
Sven Bergner <sven.bergner@gmail.com>
2026-01-29 13:18:28 +0100

    chore(nvim): Coding style

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -48,7 +48,7 @@ autocmd('BufEnter', {
 })
 
 -- Prevent Telescope from entering insert mode after leaving a prompt
-autocmd({ "BufLeave", "BufWinLeave" }, {
+autocmd({ 'BufLeave', 'BufWinLeave' }, {
    callback = function(event)
       if vim.bo[event.buf].filetype == "TelescopePrompt" then
          vim.api.nvim_exec2("silent! stopinsert!", {})
