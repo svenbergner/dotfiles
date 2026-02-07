ec018c83fbee5ae507fa0ddc0d7f2b58b86d43c7
Sven Bergner <sven.bergner@gmail.com>
2026-02-07 23:33:05 +0100

    feat(nvim/neogit): New plugin neogit added

---
+++ .config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,20 @@
+--[===[
+Neogit
+A git interface for Neovim, inspired by Magit.
+
+URL: https://github.com/NeogitOrg/neogit
+--]===]
+
+return {
+   'NeogitOrg/neogit',
+   lazy = true,
+   dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+      'folke/snacks.nvim', -- optional
+   },
+   cmd = 'Neogit',
+   keys = {
+      { '<leader>GG', '<cmd>Neogit<cr>', desc = 'Show Neogit UI' },
+   },
+}
