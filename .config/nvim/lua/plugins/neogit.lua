8cbaee51b02faebb2f428da26d93fc699240693f
Sven Bergner <sven.bergner@gmail.com>
2026-02-17 13:08:36 +0100

    feat(nvim/neogit): Make neogit floating

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -3,6 +3,9 @@ Neogit
 https://github.com/NeogitOrg/neogit
 
 A git interface for Neovim, inspired by Magit.
+
+Dependencies:
+https://github.com/esmuellert/codediff.nvim
 --]===]
 
 return {
@@ -10,10 +13,18 @@ return {
    enabled = true,
    dependencies = {
       'nvim-lua/plenary.nvim', -- required
-      'sindrets/diffview.nvim', -- optional - Diff integration
+      'esmuellert/codediff.nvim', -- optional - Diff integration
       'folke/snacks.nvim', -- optional
    },
    cmd = 'Neogit',
+   opts = {
+      kind = 'floating',
+      integrations = {
+         codediff = true, -- integrate with codediff.nvim
+         snacks = true, -- integrate with snacks.nvim
+      },
+      diff_viewer = 'codediff',
+   },
    keys = {
       { '<leader>GG', '<cmd>Neogit<cr>', desc = 'Show Neogit UI' },
    },
