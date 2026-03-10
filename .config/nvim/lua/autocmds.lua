ca102ee44d0f98292222df83b5dacf9e8c0b91e5
Sven Bergner <sven.bergner@gmail.com>
2026-03-10 10:01:31 +0100

    feat(nvim/lsp): Added autocmds for moxide_lsp

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -117,3 +117,25 @@ vim.api.nvim_create_autocmd('FileType', {
      vim.treesitter.start()
   end,
 })
+-- activate codelens if supported by any attached LSP client
+local function check_codelens_support()
+local clients = vim.lsp.get_active_clients({ bufnr = 0 })
+for _, c in ipairs(clients) do
+  if c.server_capabilities.codeLensProvider then
+    return true
+  end
+end
+return false
+end
+
+vim.api.nvim_create_autocmd({ 'TextChanged', 'InsertLeave', 'CursorHold', 'LspAttach', 'BufEnter' }, {
+buffer = bufnr,
+callback = function ()
+  if check_codelens_support() then
+    vim.lsp.codelens.refresh({bufnr = 0})
+  end
+end
+})
+
+-- trigger codelens refresh
+vim.api.nvim_exec_autocmds('User', { pattern = 'LspAttached' })
