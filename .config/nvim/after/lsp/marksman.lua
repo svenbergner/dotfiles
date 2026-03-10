6ee2997c6cc35927c1d28694bc2efe994ca2e821
Sven Bergner <sven.bergner@gmail.com>
2026-03-10 10:00:31 +0100

    chore(nvim/lsp): Disable marksman in favor for moxide_lsp

--- .config/nvim/after/lsp/marksman.lua
+++ .config/nvim/after/lsp/marksman.lua
@@ -2,8 +2,8 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#marksman
 
 return {
-   cmd = { "marksman", "server" },
-   filetypes = { "markdown", "markdown.mdx", "vimwiki" },
-   init_options = { provideFormatter = true, },
-   root_markers = { ".marksman.toml", ".git", },
+   -- cmd = { "marksman", "server" },
+   -- filetypes = { "markdown", "markdown.mdx", "vimwiki" },
+   -- init_options = { provideFormatter = true, },
+   -- root_markers = { ".marksman.toml", ".git", },
 }
