b811b423c638b644c6133441f2a54a61d97eb56c
Sven Bergner <sven.bergner@gmail.com>
2026-03-11 10:14:10 +0100

    Added user command to change old C++ include guards to #pragma once

--- .config/nvim/lua/usercmds.lua
+++ .config/nvim/lua/usercmds.lua
@@ -4,3 +4,14 @@ vim.api.nvim_create_user_command('RemoveDividerComments', function ()
    vim.api.nvim_command('write')
    vim.api.nvim_command('nohlsearch')
 end, {nargs = 0})
+
+-- Change include guards to #pragma once
+vim.api.nvim_create_user_command('ChangeIncludeGuardToPragmaOnce', function ()
+   vim.api.nvim_command('silent! %s/^#ifndef\\s\\+\\w\\+\\s*\\n#define\\s\\+\\w\\+\\s*\\n/#pragma once\\r/')
+   vim.api.nvim_command('silent! %s/\\n\\(\\s*\\n\\)*#endif[^\\n]*\\n\\?\\%$//')
+   vim.api.nvim_command('silent! %s/^#pragma once\\n\\(\\s*\\n\\)*\\(#include\\)/#pragma once\\r\\r\\2/')
+   vim.api.nvim_command('write')
+   vim.api.nvim_command('nohlsearch')
+end, {nargs = 0})
+
+
