6188a857ee0f76976058dc8960eac73ff4f2c310
Sven Bergner <sven.bergner@gmail.com>
2026-03-17 10:11:42 +0100

    refactor(nvim): Coding style

--- .config/nvim/lua/usercmds.lua
+++ .config/nvim/lua/usercmds.lua
@@ -1,17 +1,17 @@
 -- Remove divider comments
-vim.api.nvim_create_user_command('RemoveDividerComments', function ()
+vim.api.nvim_create_user_command('RemoveDividerComments', function()
    vim.api.nvim_command('silent! %s/^\\/\\/\\**\\n//')
    vim.api.nvim_command('write')
    vim.api.nvim_command('nohlsearch')
-end, {nargs = 0})
+end, { nargs = 0 })
 
 -- Change include guards to #pragma once
-vim.api.nvim_create_user_command('ChangeIncludeGuardToPragmaOnce', function ()
+vim.api.nvim_create_user_command('ChangeIncludeGuardToPragmaOnce', function()
    vim.api.nvim_command('silent! %s/^#ifndef\\s\\+\\w\\+\\s*\\n#define\\s\\+\\w\\+\\s*\\n/#pragma once\\r/')
    vim.api.nvim_command('silent! %s/\\n\\(\\s*\\n\\)*#endif[^\\n]*\\n\\?\\%$//')
    vim.api.nvim_command('silent! %s/^#pragma once\\n\\(\\s*\\n\\)*\\(#include\\)/#pragma once\\r\\r\\2/')
    vim.api.nvim_command('write')
    vim.api.nvim_command('nohlsearch')
-end, {nargs = 0})
+end, { nargs = 0 })
 
 
