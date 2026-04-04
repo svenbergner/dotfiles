d78e5457cc3370e47250164e8453dbef1211b171
Sven Bergner <sven.bergner@gmail.com>
2026-04-04 11:06:01 +0200

    chore(nvim/image): Removed image plugin

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -72,7 +72,6 @@ return {
       'nvim-lua/plenary.nvim',
       'nvim-tree/nvim-web-devicons',
       'MunifTanjim/nui.nvim',
-      '3rd/image.nvim',
       {
          's1n7ax/nvim-window-picker',
          config = function()
