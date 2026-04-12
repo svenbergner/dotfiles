9885a446bc7f1f2c282b3b2a6b98fa6c753b3b04
Sven Bergner <sven.bergner@gmail.com>
2026-04-12 15:18:50 +0200

    chore(nvim): Use vim.pack to bootstrap lazy.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,18 +25,7 @@ require('usercmds')
 require('lsp-keymaps')
 
 -- Bootstrap the lazy.nvim package manager
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.uv.fs_stat(lazypath) then
-   vim.fn.system({
-      'git',
-      'clone',
-      '--filter=blob:none',
-      'https://github.com/folke/lazy.nvim.git',
-      '--branch=stable', -- use the latest stable release
-      lazypath,
-   })
-end
-vim.opt.rtp:prepend(lazypath)
+vim.pack.add({ 'https://github.com/folke/lazy.nvim' })
 
 require('lazy').setup({
    { import = 'plugins' },
