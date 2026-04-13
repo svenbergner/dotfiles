25ea1b11b2c3ebec0aa0c59bfaeae2e9c3d9d726
Sven Bergner <sven.bergner@gmail.com>
2026-04-13 14:53:55 +0200

    chore(nvim): Cleanup config

--- .config/nvim/lua/plugins/helpview.lua
+++ .config/nvim/lua/plugins/helpview.lua
@@ -6,10 +6,5 @@ https://github.com/OXY2DEV/helpview.nvim
 return {
    'OXY2DEV/helpview.nvim',
    enabled = true,
-
    ft = 'help',
-
-   dependencies = {
-      -- 'nvim-treesitter/nvim-treesitter',
-   },
 }
