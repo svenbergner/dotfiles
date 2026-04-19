3afa579040bc82bad818989905ce2bc1c7882884
Sven Bergner <sven.bergner@gmail.com>
2026-04-19 12:06:15 +0200

    chore(nvim): Removed codelens support

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -109,26 +109,3 @@ autocmd('InsertLeave', {
       end
    end,
 })
-
--- activate codelens if supported by any attached LSP client
-local function check_codelens_support()
-   local clients = vim.lsp.get_clients({ bufnr = 0 })
-   for _, c in ipairs(clients) do
-      if c.server_capabilities.codeLensProvider then
-         return true
-      end
-   end
-   return false
-end
-
-vim.api.nvim_create_autocmd({ 'TextChanged', 'InsertLeave', 'CursorHold', 'LspAttach', 'BufEnter' }, {
-   callback = function()
-      if check_codelens_support() then
-         vim.lsp.codelens.enable(true)
-      end
-   end,
-})
-
--- trigger codelens refresh
-vim.api.nvim_exec_autocmds('User', { pattern = 'LspAttached' })
-
