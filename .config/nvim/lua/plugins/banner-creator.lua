bd253ff7dbfe79a7b0597f401db3e2c376f849f0
Sven Bergner <sven.bergner@gmail.com>
2026-05-08 17:24:09 +0200

    feat(nvim): New plugin added

---
+++ .config/nvim/lua/plugins/banner-creator.lua
@@ -0,0 +1,9 @@
+--[===[
+A Neovim plugin to generate banner-style text with figlet and boxes
+https://github.com/svenbergner/banner_creator.nvim
+--]===]
+
+return {
+   'svenbergner/banner_creator.nvim',
+   dev = true,
+}
