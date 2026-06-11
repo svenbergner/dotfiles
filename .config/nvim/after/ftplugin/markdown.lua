2b62c57e04895bfa56b187aeb8d7003c19b2f031
Sven Bergner <sven.bergner@gmail.com>
2026-06-11 13:13:13 +0200

    feat(nvim/presenter): New keymappings added

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -4,8 +4,10 @@ vim.api.nvim_create_autocmd("BufEnter", {
    group = group_id,
    pattern = "*.md",
    callback = function()
-      vim.keymap.set('n', '<F5>', '<cmd>PresenterStart<CR>',
-         { silent = true, buffer = true, desc = "Starts a presentation of the current markdownfile" })
+      vim.keymap.set('n', '<F5>', '<cmd>PresenterAudienceStart<CR>',
+         { silent = true, buffer = true, desc = "Starts a presentation of the current markdownfile for the audience" })
+      vim.keymap.set('n', '<F17>', '<cmd>PresenterSpeakerStart<CR>',
+         { silent = true, buffer = true, desc = "Starts a presentation of the current markdownfile in speaker mode" })
       vim.keymap.set('n', '<leader>mc', '^I-<Space>[<Space>]<Space><Esc>^j',
          { remap = true, silent = false, desc = "[m]arkdown [c]hackable list item" })
       vim.keymap.set('n', '<leader>ml', '^I-<Space><Esc>^j',
