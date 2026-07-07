a3eb0662105d77bf9a1cf484526bb9678401ba95
Sven Bergner <sven.bergner@gmail.com>
2026-07-07 11:32:47 +0200

    fix(nvim/lualine): Hide testing section if there is nothing to show

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -176,6 +176,8 @@ local function getTestBuildContext()
    end
 
    if total == 0 and passed == 0 and failed == 0 and skipped == 0 and running == 0 then
+      return ''
+   else
       return label
    end
 
