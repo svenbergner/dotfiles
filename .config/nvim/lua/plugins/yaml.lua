b03a798d93783fbc0047481c7921820e4b6ea113
Sven Bergner <sven.bergner@gmail.com>
2026-08-07 14:12:24 +0200

    feat(nvim/yaml): Added command documentation

--- .config/nvim/lua/plugins/yaml.lua
+++ .config/nvim/lua/plugins/yaml.lua
@@ -1,6 +1,23 @@
 --[===[
 Simple tools to help developers working YAML in Neovim
 https://github.com/cuducos/yaml.nvim
+
+Command                    Lua API                      Description
+:YAMLView                  yaml.view()                  Shows the full path and value of the current key/value pair
+:YAMLYank [register]       yaml.yank_all([register])    Yanks the full path and value of the current key/value pair.
+                                                        The default register is the unnamed one (")
+:YAMLYankKey [register]    yaml.yank_key([register])    Yanks the full path of the key for the current key/value pair.
+                                                        The default register is the unnamed one (")
+:YAMLYankValue [register]  yaml.yank_value([register])  Yanks the value of the current key/value pair.
+                                                        The default register is the unnamed one (")
+:YAMLHighlight <key>	      yaml.highlight(key)          Highlights the line(s) of an YAML key
+:YAMLRemoveHighlight	      yaml.remove_highlight()      Removes the highlight created by :YAMLHighlight/yaml.highlight(key)
+:YAMLQuickfix	            yaml.quickfix()              Generates a quickfix with key/value pairs
+:YAMLSnacks	               yaml.snacks()                Full path key/value fuzzy finder via Snacks if installed
+:YAMLTelescope	            yaml.telescope()             Full path key/value fuzzy finder via Telescope if installed
+:YAMLFzfLua	               yaml.fzf_lua()               Full path key/value fuzzy finder via fzf-lua if installed
+:YAMLSort	               yaml.sort()                  Sorts YAML keys alphabetically within the mapping under the cursor,
+                                                        recursing into nested mappings
 --]===]
 
 return {
