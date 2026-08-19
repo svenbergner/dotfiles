4454d9369ac8d0f23d9a591c9439e145c76677bc
Sven Bergner <sven.bergner@gmail.com>
2026-08-19 11:54:14 +0200

    feat(nvim/auto-session): Added several keymaps for auto-session

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -16,5 +16,14 @@ return {
          },
          suppress_dirs = { '~/', '~/Download', '/' },
       })
+      vim.keymap.set('n', '<leader>sd', '<cmd>AutoSession delete<CR>', { desc = 'auto-[s]ession [d]elete' })
+      vim.keymap.set('n', '<leader>sD', '<cmd>AutoSession <CR>', { desc = 'auto-[s]ession [D]isable' })
+      vim.keymap.set('n', '<leader>se', '<cmd>AutoSession enable<CR>', { desc = 'auto-[s]ession [e]nable' })
+      vim.keymap.set('n', '<leader>sp', '<cmd>AutoSession deletePicker<CR>', { desc = 'auto-[s]ession delete[p]icker' })
+      vim.keymap.set('n', '<leader>sP', '<cmd>AutoSession purgeOrphaned<CR>', { desc = 'auto-[s]ession purge[o]rphaned' })
+      vim.keymap.set('n', '<leader>sr', '<cmd>AutoSession restore<CR>', { desc = 'auto-[s]ession [r]estore' })
+      vim.keymap.set('n', '<leader>sS', '<cmd>AutoSession search<CR>', { desc = 'auto-[s]ession [S]earch' })
+      vim.keymap.set('n', '<leader>st', '<cmd>AutoSession toggle<CR>', { desc = 'auto-[s]ession [t]oggle' })
+      vim.keymap.set('n', '<leader>ss', '<cmd>AutoSession save<CR>', { desc = 'auto-[s]ession [s]ave' })
    end,
 }
