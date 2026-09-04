f4f880c742779e5354a6207baab6bee46d133667
Sven Bergner <sven.bergner@gmail.com>
2026-09-04 19:44:42 +0200

    feat(nvim): Save cursor posiiton and return there afterwards

--- .config/nvim/lua/usercmds.lua
+++ .config/nvim/lua/usercmds.lua
@@ -1,17 +1,21 @@
 -- Remove divider comments
 vim.api.nvim_create_user_command('RemoveDividerComments', function()
+   local view = vim.fn.winsaveview()
    vim.api.nvim_command('silent! %s/^\\/\\/\\**\\n//')
    vim.api.nvim_command('write')
    vim.api.nvim_command('nohlsearch')
+   vim.fn.winrestview(view)
 end, { nargs = 0 })
 
 -- Change include guards to #pragma once
 vim.api.nvim_create_user_command('ChangeIncludeGuardToPragmaOnce', function()
+   local view = vim.fn.winsaveview()
    vim.api.nvim_command('silent! %s/^#ifndef\\s\\+\\w\\+\\s*\\n#define\\s\\+\\w\\+\\s*\\n/#pragma once\\r/')
    vim.api.nvim_command('silent! %s/\\n\\(\\s*\\n\\)*#endif[^\\n]*\\n\\?\\%$//')
    vim.api.nvim_command('silent! %s/^#pragma once\\n\\(\\s*\\n\\)*\\(#include\\)/#pragma once\\r\\r\\2/')
    vim.api.nvim_command('write')
    vim.api.nvim_command('nohlsearch')
+   vim.fn.winrestview(view)
 end, { nargs = 0 })
 
 -- Write quickfix list to a file
